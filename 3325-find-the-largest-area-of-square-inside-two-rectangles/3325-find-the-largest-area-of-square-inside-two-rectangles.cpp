b5950a8f2e395226ac8cfd3e69acb4a38f767cc4
Nikitiwe <rvnggm@gmail.com>
2024-06-22 18:20:39 +0700

    Time: 182 ms (81.54%), Space: 58.8 MB (10.74%) - LeetHub

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/3325-find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/3325-find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -7,8 +7,11 @@ public:
             for (int j=i+1; j!=bottomLeft.size(); j++)
             {
                 deltax=min(topRight[i][0],topRight[j][0])-max(bottomLeft[i][0],bottomLeft[j][0]);
-                deltay=min(topRight[i][1],topRight[j][1])-max(bottomLeft[i][1],bottomLeft[j][1]);
-                if (deltax>0&&deltay>0) {if (ans<min(deltax,deltay)) ans=min(deltax,deltay);}
+                if (deltax>0)
+                {
+                    deltay=min(topRight[i][1],topRight[j][1])-max(bottomLeft[i][1],bottomLeft[j][1]);
+                    if (deltay>0) {if (ans<min(deltax,deltay)) ans=min(deltax,deltay);}
+                }
             }
         }
         return ans*ans;
