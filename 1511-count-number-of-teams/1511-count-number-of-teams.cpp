9f6e9638b4657e69e636099dd4507a1531db551f
Nikitiwe <rvnggm@gmail.com>
2024-06-24 22:10:44 +0700

    Time: 2019 ms (21.28%), Space: 11.4 MB (61.36%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -6,10 +6,15 @@ public:
         {
             for (int j=i+1; j!=rating.size()-1; j++)
             {
+                if (rating[i]>rating[j])
                 for (int k=j+1; k!=rating.size(); k++)
                 {
-                    if (rating[i]>rating[j]&&rating[j]>rating[k]) ans++;
-                    else if (rating[i]<rating[j]&&rating[j]<rating[k]) ans++;
+                    if (rating[j]>rating[k]) ans++;
+                }
+                else
+                for (int k=j+1; k!=rating.size(); k++)
+                {
+                    if (rating[j]<rating[k]) ans++;
                 }
             }
         }
