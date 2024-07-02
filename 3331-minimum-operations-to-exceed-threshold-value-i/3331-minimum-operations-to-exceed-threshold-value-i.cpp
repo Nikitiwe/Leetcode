b07ad6592a4017378ecb3a6cb28976ca269cd268
Nikitiwe <rvnggm@gmail.com>
2024-07-03 04:24:13 +0700

    Time: 7 ms (51.27%), Space: 23.6 MB (94.66%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
         int ans=0;
         for (int i=0; i!=nums.size(); i++)
         {
             if (nums[i]<k) ans++;
-            else break;
         }
         return ans;
     }
