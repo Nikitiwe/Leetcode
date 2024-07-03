8a6e22ca695c90c5f7a5b399b9dc9e6cc38dde65
Nikitiwe <rvnggm@gmail.com>
2024-07-03 17:38:31 +0700

    Time: 71 ms (45.15%), Space: 38.8 MB (50.84%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -3,9 +3,9 @@ public:
     int minDifference(vector<int>& nums) {
         if (nums.size()<5) return 0;
         sort(nums.begin(), nums.end());
-        return min(nums[nums.size()-1]-nums[3], min(
-                       nums[nums.size()-2]-nums[2], min(
-                       nums[nums.size()-3]-nums[1], 
-                       nums[nums.size()-4]-nums[0] )));
+        return min({nums[nums.size()-1]-nums[3], 
+                    nums[nums.size()-2]-nums[2], 
+                    nums[nums.size()-3]-nums[1], 
+                    nums[nums.size()-4]-nums[0] });
     }
 };
