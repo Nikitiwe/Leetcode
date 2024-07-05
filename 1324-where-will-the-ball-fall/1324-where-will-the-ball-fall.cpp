2f2d5389f1d6a053c4ebf49f0206802960a69554
Nikitiwe <rvnggm@gmail.com>
2024-07-05 15:24:23 +0700

    Time: 23 ms (7.93%), Space: 15.8 MB (31.16%) - LeetHub

--- 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.cpp
+++ 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.cpp
@@ -10,10 +10,16 @@ public:
             {
                 if (x==0&&grid[j][x]==-1) {ans.push_back(-1); break;}
                 else if (x==grid[0].size()-1&&grid[j][x]==1) {ans.push_back(-1); break;}
-                else if (grid[j][x]==1&&grid[j][x+1]==-1) {ans.push_back(-1); break;}
-                else if (grid[j][x]==-1&&grid[j][x-1]==1) {ans.push_back(-1); break;}
-                else if (grid[j][x]==1&&grid[j][x+1]==1) {x++;}
-                else if (grid[j][x]==-1&&grid[j][x-1]==-1) {x--;}
+                else if (grid[j][x]==1)
+                {
+                    if (grid[j][x+1]==-1) {ans.push_back(-1); break;}
+                    else {x++;}
+                } 
+                else if (grid[j][x]==-1)
+                {
+                    if (grid[j][x-1]==1) {ans.push_back(-1); break;}
+                    else {x--;}
+                }
                 if (j==grid.size()-1) ans.push_back(x);
             }
         }
