c9bac907333f74afc097859d9bd05f9d1075cfc3
Nikitiwe <rvnggm@gmail.com>
2024-07-05 20:30:08 +0700

    Time: 76 ms (46.81%), Space: 24.1 MB (81.99%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -8,10 +8,15 @@ public:
             w[word[i]-'a']++;
         }
         sort(w.begin(), w.end());
-        int ans=0, count=0;
+        int ans=0, count=0, q=1;
         for (int i=25; i>=0; i--)
         {
-            if (w[i]!=0) {count++; ans=ans+w[i]*((count+7)/8);}
+            if (w[i]!=0)
+            {
+                count++;
+                if (count==9||count==17||count==25) q++;
+                ans=ans+w[i]*q;
+            }
             else break;
         }
         return ans;
