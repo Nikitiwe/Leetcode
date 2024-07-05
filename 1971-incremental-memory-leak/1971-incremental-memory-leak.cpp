0f018067ced1386668449239f71bca0907136451
Nikitiwe <rvnggm@gmail.com>
2024-07-05 13:28:02 +0700

    Time: 4 ms (66.46%), Space: 7.4 MB (35.37%) - LeetHub

--- 1971-incremental-memory-leak/1971-incremental-memory-leak.cpp
+++ 1971-incremental-memory-leak/1971-incremental-memory-leak.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<int> memLeak(int memory1, int memory2) {
-        vector<int> ans;
         int i=1;
-        while (memory1>=0&&memory2>=0)
+        while (1)
         {
             if (memory1>=memory2) if (memory1-i>=0) memory1=memory1-i; else break;
             else if (memory2-i>=0) memory2=memory2-i; else break;
