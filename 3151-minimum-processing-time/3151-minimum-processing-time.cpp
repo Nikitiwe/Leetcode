9f3e013516c55a374d9b8bc7a363f9ce873f7a3d
Nikitiwe <rvnggm@gmail.com>
2024-07-06 18:00:27 +0700

    Time: 144 ms (42.42%), Space: 102.2 MB (49.84%) - LeetHub

--- 3151-minimum-processing-time/3151-minimum-processing-time.cpp
+++ 3151-minimum-processing-time/3151-minimum-processing-time.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) {
         sort(tasks.begin(), tasks.end());
-        sort(processorTime.rbegin(), processorTime.rend());
+        sort(processorTime.begin(), processorTime.end());
         int m=0;
         for (int i=0; i!=tasks.size(); i++)
         {
-            m=max(m, tasks[i]+processorTime[i/4]);
+            m=max(m, tasks[tasks.size()-1-i]+processorTime[i/4]);
         }
         return m;
     }
