8a35635c5930d8eb9d571aeb4ec7fb0b6deadf42
Nikitiwe <rvnggm@gmail.com>
2024-07-07 22:44:00 +0700

    Time: 130 ms (9.39%), Space: 109.8 MB (53.26%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans=0, temp=0;
+        long long ans=0;
+        int temp=0;
         for (int i=0; i!=nums.size(); i++)
         {
-            if (nums[i]==0) temp++;
-            else {ans+=temp*(temp+1)/2; temp=0;}
+            if (nums[i]==0) {temp++; ans+=temp;}
+            else temp=0;
         }
-        ans+=temp*(temp+1)/2;
         return ans;
     }
 };
