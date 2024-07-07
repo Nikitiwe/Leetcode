bafd9fe7643492c4341c5c221ada8a5dd96c8c85
Nikitiwe <rvnggm@gmail.com>
2024-07-07 17:34:52 +0700

    Time: 4 ms (65.34%), Space: 7.1 MB (83.47%) - LeetHub

---
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int a3=max({a, b, c}), a1=min({a, b, c}), a2=a+b+c-a1-a3;
+        int ans=0;
+        while (a2>0)
+        {
+            a3--;
+            a2--;
+            ans++;
+            if (a2<a1) swap(a2, a1);
+            if (a3<a2) swap(a3, a2);
+        }
+        return ans;
+    }
+};
