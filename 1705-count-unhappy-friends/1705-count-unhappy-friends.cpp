6f2ae1aeb285912137825c7c8076f7dc17b92e01
Nikitiwe <rvnggm@gmail.com>
2024-07-12 19:16:36 +0700

    Time: 45 ms (48.41%), Space: 26.5 MB (91.08%) - LeetHub

--- 1705-count-unhappy-friends/1705-count-unhappy-friends.cpp
+++ 1705-count-unhappy-friends/1705-count-unhappy-friends.cpp
@@ -7,23 +7,27 @@ public:
         {
             for (int j=0; j!=pairs.size(); j++)
             {
+                if (isit[pairs[i][0]]==0||isit[pairs[j][0]]==0) {
                 if (find(pref[pairs[i][0]].begin(), pref[pairs[i][0]].end(), pairs[i][1])>
                     find(pref[pairs[i][0]].begin(), pref[pairs[i][0]].end(), pairs[j][0])
                     &&
                     find(pref[pairs[j][0]].begin(), pref[pairs[j][0]].end(), pairs[j][1])>
                     find(pref[pairs[j][0]].begin(), pref[pairs[j][0]].end(), pairs[i][0])) { isit[pairs[i][0]]=1; isit[pairs[j][0]]=1; }
-                    
+                }
+                if (isit[pairs[i][1]]==0||isit[pairs[j][1]]==0) {
                 if (find(pref[pairs[i][1]].begin(), pref[pairs[i][1]].end(), pairs[i][0])>
                     find(pref[pairs[i][1]].begin(), pref[pairs[i][1]].end(), pairs[j][1])
                     &&
                     find(pref[pairs[j][1]].begin(), pref[pairs[j][1]].end(), pairs[j][0])>
                     find(pref[pairs[j][1]].begin(), pref[pairs[j][1]].end(), pairs[i][1])) { isit[pairs[i][1]]=1; isit[pairs[j][1]]=1; }
-                    
+                }
+                if (isit[pairs[i][0]]==0||isit[pairs[j][1]]==0) {
                 if (find(pref[pairs[i][0]].begin(), pref[pairs[i][0]].end(), pairs[i][1])>
                     find(pref[pairs[i][0]].begin(), pref[pairs[i][0]].end(), pairs[j][1])
                     &&
                     find(pref[pairs[j][1]].begin(), pref[pairs[j][1]].end(), pairs[j][0])>
                     find(pref[pairs[j][1]].begin(), pref[pairs[j][1]].end(), pairs[i][0])) { isit[pairs[i][0]]=1; isit[pairs[j][1]]=1; }
+                }
             }
         }
         for (int i=0; i!=isit.size(); i++) if (isit[i]==1) ans++;
