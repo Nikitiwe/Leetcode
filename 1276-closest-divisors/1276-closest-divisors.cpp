dc05d03fe028632bf9942b47dd37288f97a8d848
Nikitiwe <rvnggm@gmail.com>
2024-07-13 02:44:54 +0700

    Time: 8 ms (57.95%), Space: 7.7 MB (6.62%) - LeetHub

---
+++ 1276-closest-divisors/1276-closest-divisors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> closestDivisors(int num) {
+        int ans1, ans2, a, b;
+        a=sqrt(num+1);
+        while (1)
+        {
+            if ((num+1)%a==0) {ans1=a; ans2=(num+1)/a; break;}
+            else a--;
+        }
+        a=sqrt(num+2);
+        while (1)
+        {
+            if ((num+2)%a==0) {if((num+2)/a-a<ans2-ans1) {ans1=a; ans2=(num+2)/a;} else break;}
+            else a--;
+        }
+        return {ans1, ans2};
+    }
+};
