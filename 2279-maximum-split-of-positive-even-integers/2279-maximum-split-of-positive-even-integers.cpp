658f395991f5ff8fb462826f038f3305bc439e16
Nikitiwe <rvnggm@gmail.com>
2024-07-15 01:21:00 +0700

    Time: 97 ms (7.07%), Space: 44.2 MB (6.54%) - LeetHub

--- 2279-maximum-split-of-positive-even-integers/2279-maximum-split-of-positive-even-integers.cpp
+++ 2279-maximum-split-of-positive-even-integers/2279-maximum-split-of-positive-even-integers.cpp
@@ -3,7 +3,12 @@ public:
     vector<long long> maximumEvenSplit(long long finalSum) {
         if (finalSum%2==1) return {};
         vector<long long> ans;
-        int i=2;
+        long long i=2;
+        for (i; i<sqrt(finalSum/2); i=i+2)
+        {
+            ans.push_back(i);
+            finalSum-=i;
+        }
         while (2*i+2<=finalSum)
         {
             ans.push_back(i);
