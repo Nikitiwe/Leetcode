ac641d90398a18764615d53ffcff009d2b24e13c
Nikitiwe <rvnggm@gmail.com>
2024-07-15 11:59:14 +0700

    Time: 113 ms (56.89%), Space: 106 MB (19.56%) - LeetHub

--- 2233-number-of-smooth-descent-periods-of-a-stock/2233-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2233-number-of-smooth-descent-periods-of-a-stock/2233-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
-        long long ans=1, temp=1;
+        long long ans=0, temp=1;
         for (int i=1; i!=prices.size(); i++)
         {
             if (prices[i]==prices[i-1]-1) temp++;
-            else temp=1;
-            ans+=temp;
+            else {ans+=temp*(temp+1)/2; temp=1;}
         }
+        ans+=temp*(temp+1)/2;
         return ans;
     }
 };
