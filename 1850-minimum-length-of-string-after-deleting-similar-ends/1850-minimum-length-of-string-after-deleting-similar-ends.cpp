3691213db5422f98288e3f2b727574772eecc392
Nikitiwe <rvnggm@gmail.com>
2024-07-17 18:26:24 +0700

    Time: 22 ms (81.73%), Space: 14.2 MB (6.95%) - LeetHub

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/1850-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        if (s.size()==1) return 1;
+        int l=0, r=s.size()-1;
+        while (l<r)
+        {
+            if (s[l]!=s[r]) break;
+            while (l<s.size()-1&&s[l+1]==s[l]) l++;
+            while (r>=1&&s[r-1]==s[r]) r--;
+            if (l<s.size()-1) l++;
+            if (r>=1) r--;
+        }
+        return max(0, r-l+1);
+    }
+};
