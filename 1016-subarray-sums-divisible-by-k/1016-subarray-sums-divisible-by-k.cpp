df72970257432cedc46a9e7a6b9e246bc1a3f1f1
Nikitiwe <rvnggm@gmail.com>
2024-07-18 00:35:55 +0700

    Time: 33 ms (78.03%), Space: 33.9 MB (75.27%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        vector<int> sums(nums.size()+1, 0);
+        vector<int> sums(nums.size()+1, 0), count(k, 0);
+        count[0]++;
         for (int i=0; i!=nums.size(); i++)
         {
             sums[i+1]=(sums[i]+nums[i])%k;
             sums[i+1]=(sums[i+1]+k)%k;
+            count[sums[i+1]]++;
         }
-        int ans=0, temp;
+        int ans=0;
         for (int i=0; i!=k; i++)
         {
-            temp=0;
-            for (int j=0; j!=sums.size(); j++)
-            {
-                if (sums[j]==i) temp++;
-            }
-            ans+=temp*(temp-1)/2;
+            ans+=count[i]*(count[i]-1)/2;
         }
         return ans;
     }
