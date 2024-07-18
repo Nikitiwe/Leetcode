bd06d1654f8530795fc72de37bce72101e84ca94
Nikitiwe <rvnggm@gmail.com>
2024-07-19 02:26:58 +0700

    Time: 165 ms (42.76%), Space: 107.8 MB (27.78%) - LeetHub

---
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end());
+        long long ans=0;
+        for (int i=0; i!=happiness.size()&&i<k; i++)
+        {
+            ans+=max(happiness[happiness.size()-1-i]-i, 0);
+        }
+        return ans;
+    }
+};
