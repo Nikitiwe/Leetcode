161a2de0273c7a3f43760f3b2b8b2c3099ab459f
Nikitiwe <rvnggm@gmail.com>
2024-07-23 00:42:22 +0700

    Time: 184 ms (71.49%), Space: 89.8 MB (28.51%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -10,12 +10,13 @@ public:
             }
         }
         sort(arr.begin(), arr.end());
-        int ans=0, l=0, r=arr.size()-1;
+        int ans=0, l=0, r=arr.size()-1, temp;
         if (l==r) return 0;
         while (l<r)
         {
-            if ((arr[r]-arr[l])%x!=0||(arr[l+1]-arr[l])%x!=0) return -1;
-            ans+=((arr[r]-arr[l])/x);
+            temp=arr[r]-arr[l];
+            if (temp%x!=0||(arr[l+1]-arr[l])%x!=0) return -1;
+            ans+=(temp/x);
             l++;
             r--;
         }
