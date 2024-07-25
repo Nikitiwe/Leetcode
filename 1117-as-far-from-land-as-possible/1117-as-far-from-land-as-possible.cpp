568af90c8438d58172b495e6a1c185a05f63f802
Nikitiwe <rvnggm@gmail.com>
2024-07-26 00:48:17 +0700

    Time: 33 ms (95.49%), Space: 19.5 MB (97.05%) - LeetHub

--- 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/1117-as-far-from-land-as-possible.cpp
@@ -7,25 +7,25 @@ public:
             for (int j=0; j!=n; j++)
             {
                 if (j>0&&grid[i][j-1]>0&&grid[i][j]!=1) grid[i][j]=grid[i][j-1]+1;
-                if (i>0&&grid[i-1][j]>0&&grid[i][j]!=1&&grid[i][j]!=0) grid[i][j]=min(grid[i][j], grid[i-1][j]+1);
-                else if (i>0&&grid[i-1][j]>0&&grid[i][j]!=1&&grid[i][j]==0) grid[i][j]=grid[i-1][j]+1;
+                if (i>0&&grid[i-1][j]>0&&grid[i][j]>1) grid[i][j]=min(grid[i][j], grid[i-1][j]+1);
+                else if (i>0&&grid[i-1][j]>0&&grid[i][j]==0) grid[i][j]=grid[i-1][j]+1;
             }
         }
         for (int i=n-1; i>=0; i--)
         {
             for (int j=n-1; j>=0; j--)
             {
-                if (grid[i][j]!=0&&grid[i][j]!=1)
+                if (grid[i][j]>1)
                 {
                     if (j<n-1&&grid[i][j+1]+1<grid[i][j]) grid[i][j]=grid[i][j+1]+1;
                     if (i<n-1&&grid[i+1][j]+1<grid[i][j]) grid[i][j]=min(grid[i][j], grid[i+1][j]+1);
                     ans=max(ans, grid[i][j]);
                 }
-                if (grid[i][j]==0)
+                else if (grid[i][j]==0)
                 {
                     if (j<n-1&&grid[i][j+1]>0) grid[i][j]=grid[i][j+1]+1;
-                    if (i<n-1&&grid[i+1][j]>0&&grid[i][j]!=1&&grid[i][j]!=0) grid[i][j]=min(grid[i][j], grid[i+1][j]+1);
-                    else if (i<n-1&&grid[i+1][j]>0&&grid[i][j]!=1&&grid[i][j]==0) grid[i][j]=grid[i+1][j]+1;
+                    if (i<n-1&&grid[i+1][j]>0&&grid[i][j]>1) grid[i][j]=min(grid[i][j], grid[i+1][j]+1);
+                    else if (i<n-1&&grid[i+1][j]>0) grid[i][j]=grid[i+1][j]+1;
                     ans=max(ans, grid[i][j]);
                 }
             }
