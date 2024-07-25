2b0acaabf31b57c1477819c7e8264f22aa20e180
Nikitiwe <rvnggm@gmail.com>
2024-07-25 20:50:03 +0700

    Time: 223 ms (79.44%), Space: 180.9 MB (87.8%) - LeetHub

---
+++ 2834-relocate-marbles/2834-relocate-marbles.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+        unordered_set<int> m;
+        for (int i=0; i!=nums.size(); i++)
+        {
+            m.insert(nums[i]);
+        }
+        for (int i=0; i!=moveFrom.size(); i++)
+        {
+            m.erase(moveFrom[i]);
+            m.insert(moveTo[i]);
+        }
+        nums.clear();
+        for (auto it=m.begin(); it!=m.end(); it++)
+        {
+            nums.push_back(*it);
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
