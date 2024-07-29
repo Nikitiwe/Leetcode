2c071dcf6dab3d6a35154d7c7ef6ce81d8253fe5
Nikitiwe <rvnggm@gmail.com>
2024-07-29 22:57:31 +0700

    Time: 98 ms (93.25%), Space: 38.4 MB (29.96%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long ans=0;
-        int m=999999, count=0;
+        int m=999999, count=0, t;
         for (int i=0; i!=matrix.size(); i++)
         {
             for (int j=0; j!=matrix[0].size(); j++)
             {
-                ans+=abs(matrix[i][j]);
-                m=min(m, abs(matrix[i][j]));
+                t=abs(matrix[i][j]);
+                ans+=t;
+                m=min(m, t);
                 if (matrix[i][j]<0) count++;
             }
         }
