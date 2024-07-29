67b71c95091f09c0967770d06e57136add6ed742
Nikitiwe <rvnggm@gmail.com>
2024-07-29 15:58:47 +0700

    Time: 162 ms (98.58%), Space: 215.7 MB (79.3%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -20,24 +20,24 @@ public:
                 t[i]=999999;
                 m[i]=m[i-1];
                 b[i]=b[i-1];
-                m[i]=min(m[i], min(t[i], b[i])+1);
-                b[i]=min(b[i], min(t[i], m[i])+1);
+                m[i]=min(m[i], b[i]+1);
+                b[i]=min(b[i], m[i]+1);
             }
             else if (obstacles[i]==2)
             {
                 t[i]=t[i-1];
                 m[i]=999999;
                 b[i]=b[i-1];
-                t[i]=min(t[i], min(m[i], b[i])+1);
-                b[i]=min(b[i], min(t[i], m[i])+1);
+                t[i]=min(t[i], b[i]+1);
+                b[i]=min(b[i], t[i]+1);
             }
             else
             {
                 t[i]=t[i-1];
                 m[i]=m[i-1];
                 b[i]=999999;
-                t[i]=min(t[i], min(m[i], b[i])+1);
-                m[i]=min(m[i], min(t[i], b[i])+1);
+                t[i]=min(t[i], m[i]+1);
+                m[i]=min(m[i], t[i]+1);
             }
         }
         return min({t[obstacles.size()-1], m[obstacles.size()-1], b[obstacles.size()-1]});
