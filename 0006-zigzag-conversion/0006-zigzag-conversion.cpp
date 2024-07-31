833d1b9631e6f91c23231e9f79c1d65db540a02b
Nikitiwe <rvnggm@gmail.com>
2024-07-31 20:09:14 +0700

    Time: 12 ms (53.36%), Space: 13.4 MB (59.03%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -3,12 +3,12 @@ public:
     string convert(string s, int n) {
         if (n==1) return s;
         vector<string> ans(min(n, (int)s.size()), "");
-        int temp;
+        int temp, k=2*n-2;
         for (int i=0; i!=s.size(); i++)
         {
-            temp=i%(2*n-2);
+            temp=i%k;
             if (temp<n) ans[temp]+=s[i];
-            else ans[2*n-2-temp]+=s[i];
+            else ans[k-temp]+=s[i];
         }
         s="";
         for (int i=0; i!=ans.size(); i++)
