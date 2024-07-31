b658f3acd5df6bdd257e839d0dc23c75ef1df86b
Nikitiwe <rvnggm@gmail.com>
2024-07-31 23:08:44 +0700

    Time: 160 ms (5.25%), Space: 62.2 MB (8.14%) - LeetHub

---
+++ 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        unordered_map<int, long long> m;
+        long long ans=0, temp;
+        string s;
+        for (int i=0; i!=nums.size(); i++)
+        {
+            s=to_string(nums[i]);
+            reverse(s.begin(), s.end());
+            temp=nums[i]-stoi(s);
+            if (m.count(temp)==0) m.insert({temp, 1});
+            else m[temp]++;
+        }
+        for (auto it=m.begin(); it!=m.end(); it++)
+        {
+            temp=it->second;
+            ans+=temp*(temp-1)/2;
+            ans%=1000000007;
+        }
+        return ans;
+    }
+};
