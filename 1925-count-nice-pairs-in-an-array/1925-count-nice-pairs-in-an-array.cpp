dd2c001212f9ba6f3182548f2f09eea5410da690
Nikitiwe <rvnggm@gmail.com>
2024-07-31 23:11:04 +0700

    Time: 171 ms (5.25%), Space: 62.3 MB (7.71%) - LeetHub

--- 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/1925-count-nice-pairs-in-an-array.cpp
@@ -10,14 +10,15 @@ public:
             reverse(s.begin(), s.end());
             temp=nums[i]-stoi(s);
             if (m.count(temp)==0) m.insert({temp, 1});
-            else m[temp]++;
+            else {m[temp]++; ans+=m[temp]-1;}
+            ans%=1000000007;
         }
-        for (auto it=m.begin(); it!=m.end(); it++)
+        /*for (auto it=m.begin(); it!=m.end(); it++)
         {
             temp=it->second;
             ans+=temp*(temp-1)/2;
             ans%=1000000007;
-        }
+        }*/
         return ans;
     }
 };
