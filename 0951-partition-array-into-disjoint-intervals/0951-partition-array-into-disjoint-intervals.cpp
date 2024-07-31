d7e38a042944c24f1bf9061d6495faa5e6e859d1
Nikitiwe <rvnggm@gmail.com>
2024-07-31 20:29:45 +0700

    Time: 96 ms (89.33%), Space: 101 MB (45.78%) - LeetHub

--- 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
+++ 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        vector<int> lmax(nums.size(), 0), rmin(nums.size(), 0);
-        lmax[0]=nums[0];
-        for (int i=1; i!=nums.size(); i++)
-        {
-            lmax[i]=max(lmax[i-1], nums[i]);
-        }
-        rmin[nums.size()-1]=nums[nums.size()-1];
+        vector<int> arr(nums.size(), 0);
+        arr[nums.size()-1]=nums[nums.size()-1];
         for (int i=nums.size()-2; i>=0; i--)
         {
-            rmin[i]=min(rmin[i+1], nums[i]);
+            arr[i]=min(arr[i+1], nums[i]);
         }
-        for (int i=0; i!=nums.size()-1; i++)
+        arr[0]=nums[0];
+        if (arr[0]<=arr[1]) return 1;
+        for (int i=1; i!=nums.size(); i++)
         {
-            if (lmax[i]<=rmin[i+1]) return i+1;
+            arr[i]=max(arr[i-1], nums[i]);
+            if (arr[i]<=arr[i+1]) return i+1;
         }
         return 0;
     }
