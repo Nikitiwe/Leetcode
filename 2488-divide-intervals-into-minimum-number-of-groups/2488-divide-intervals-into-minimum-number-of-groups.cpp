69f0db4a96aeae16a8bcd69a31ce74088da0912d
Nikitiwe <rvnggm@gmail.com>
2024-08-06 23:06:05 +0700

    Time: 288 ms (56.36%), Space: 204.1 MB (14.39%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        int l=intervals[0][0], r=0, ans;
+        int l=intervals[0][0], lm=0, r=0, ans;
         for(int i=0; i!=intervals.size(); i++)
         {
             l=min(l, intervals[i][0]);
+            lm=max(lm, intervals[i][0]);
             r=max(r, intervals[i][1]);
         }
         vector<int> arr(r-l+2, 0);
@@ -14,10 +15,11 @@ public:
             arr[intervals[i][1]+1-l]--;
         }
         ans=arr[0];
-        for(int i=1; i<=r-l+1; i++)
+        for(int i=1; i<=lm-l+1; i++)
         {
             arr[i]+=arr[i-1];
             ans=max(ans, arr[i]);
+            if (ans==intervals.size()) break;
         }
         return ans;
     }
