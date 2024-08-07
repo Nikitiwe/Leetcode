fefb2e83c690381ec8ab7cc71469f4dc60306096
Nikitiwe <rvnggm@gmail.com>
2024-08-08 05:43:27 +0700

    Time: 79 ms (93.38%), Space: 74 MB (41.91%) - LeetHub

---
+++ 2530-minimize-maximum-of-array/2530-minimize-maximum-of-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long ans=0, sum=0;
+        for (int i=0; i!=nums.size(); i++)
+        {
+            sum+=nums[i];
+            ans=max(ans, (sum+i)/(i+1));
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        int temp, f=1;
+        while (f==1)
+        {
+            for (int i=nums.size()-1; i>0; i--)
+            {
+                f=0;
+                if (nums[i-1]<nums[i])
+                {
+                    f=1;
+                    temp=(nums[i]-nums[i-1])/2;
+                    nums[i-1]+=temp;
+                    nums[i]-=temp;
+                    if (nums[i-1]<nums[i])
+                    {
+                        nums[i-1]++;
+                        nums[i]--;
+                    }
+                }
+            }
+        }
+        return *max_element(nums.begin(), nums.end());
+    }
+};
+*/
