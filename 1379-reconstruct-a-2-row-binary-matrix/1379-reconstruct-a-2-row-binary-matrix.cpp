d61ec218fd8e506576b1f53745d921612f7620d7
Nikitiwe <rvnggm@gmail.com>
2024-08-07 18:57:45 +0700

    Time: 43 ms (74.58%), Space: 63.2 MB (40.52%) - LeetHub

--- 1379-reconstruct-a-2-row-binary-matrix/1379-reconstruct-a-2-row-binary-matrix.cpp
+++ 1379-reconstruct-a-2-row-binary-matrix/1379-reconstruct-a-2-row-binary-matrix.cpp
@@ -12,20 +12,24 @@ public:
                 lower--;
             }
         }
+        if (upper<0) return {};
         for (int i=0; i!=colsum.size(); i++)
         {
-            if (colsum[i]==1&&upper>0)
+            if (colsum[i]==1)
             {
-                ans[0][i]=1;
-                upper--;
-            }
-            else if (colsum[i]==1)
-            {
-                ans[1][i]=1;
-                lower--;
+                if (upper>0)
+                {
+                    ans[0][i]=1;
+                    upper--;
+                }
+                else
+                {
+                    ans[1][i]=1;
+                    lower--;
+                }
             }
         }
-        if (lower!=0||upper<0) return {};
+        if (lower!=0) return {};
         else return ans;
     }
 };
