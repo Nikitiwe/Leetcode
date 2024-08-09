1f4c7142d35ab9c85d268b66130ab8afdeba2262
Nikitiwe <rvnggm@gmail.com>
2024-08-10 02:49:10 +0700

    Time: 35 ms (9.89%), Space: 46.7 MB (26.15%) - LeetHub

--- 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums1, vector<int>& nums2) {
-        int bad11=0, bad12=0, bad21=0, bad22=0, n=nums1.size()-1;
+        int bad11=0, bad12=0, bad21=0, bad22=0, n=nums1.size()-1, temp;
         for (int i=0; i<n; i++)
         {
-            if (nums1[i]>nums1[n]) {bad11++; if (nums2[i]>nums1[n]||nums1[i]>nums2[n]) return -1;}
-            if (nums2[i]>nums2[n]) {bad22++; if (nums1[i]>nums2[n]||nums2[i]>nums1[n]) return -1;}
-            if (nums1[i]>nums2[n]) bad12++;
-            if (nums2[i]>nums1[n]) bad21++;
+            temp=0;
+            if (nums1[i]>nums1[n]) {bad11++; temp++;}
+            if (nums2[i]>nums2[n]) {bad22++; temp++;}
+            if (nums1[i]>nums2[n]) {bad12++; temp++;}
+            if (nums2[i]>nums1[n]) {bad21++; temp++;}
+            if (temp>1) return -1;
         }
     return min(bad22+bad11, 1+bad21+bad12);
     }
