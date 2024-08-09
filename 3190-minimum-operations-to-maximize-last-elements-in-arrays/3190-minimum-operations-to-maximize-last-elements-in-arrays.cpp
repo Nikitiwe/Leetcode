43cf4ab61146ff1c021f0629e7145952b4a65814
Nikitiwe <rvnggm@gmail.com>
2024-08-10 02:43:07 +0700

    Time: 32 ms (18.02%), Space: 46.6 MB (26.15%) - LeetHub

--- 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -4,12 +4,10 @@ public:
         int bad11=0, bad12=0, bad21=0, bad22=0, n=nums1.size()-1;
         for (int i=0; i<n; i++)
         {
-            if (nums1[i]>nums1[n]) if (nums2[i]>nums1[n]||nums1[i]>nums2[n]) return -1;
-            if (nums2[i]>nums2[n]) if (nums1[i]>nums2[n]||nums2[i]>nums1[n]) return -1;
-            if (nums1[i]>nums1[n]) bad11++;
+            if (nums1[i]>nums1[n]) {bad11++; if (nums2[i]>nums1[n]||nums1[i]>nums2[n]) return -1;}
+            if (nums2[i]>nums2[n]) {bad22++; if (nums1[i]>nums2[n]||nums2[i]>nums1[n]) return -1;}
             if (nums1[i]>nums2[n]) bad12++;
             if (nums2[i]>nums1[n]) bad21++;
-            if (nums2[i]>nums2[n]) bad22++;
         }
     return min(bad22+bad11, 1+bad21+bad12);
     }
