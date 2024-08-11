46e2e9f797c9b60a216af0d4af39fece3fd0da4a
Nikitiwe <rvnggm@gmail.com>
2024-08-12 00:32:57 +0700

    Time: 4 ms (66.67%), Space: 8.2 MB (76.04%) - LeetHub

---
+++ 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxRepOpt1(string text) {
+        int l=0, r=0, ans=0, sum=0, temp=0, diff=0, gans=1;
+        for (int i='a'; i<='z'; i++)
+        {
+            l=0; r=0; sum=0; temp=0; diff=0; ans=0;
+            for (int j=0; j!=text.size(); j++) if (text[j]==i) sum++;
+            if (text[r]==i) temp++;
+            else diff++;
+            if (sum>0) while (r<text.size())
+            {
+                if (diff<=1)
+                {
+                    r++;
+                    if (r==text.size()) break;
+                    if (text[r]==i) {temp++; ans=max(ans, temp);}
+                    else diff++;
+                }
+                else
+                {
+                    if (text[l]==i) temp--;
+                    else diff--;
+                    l++;
+                }
+            }
+            if (sum>ans) ans++;
+            gans=max(gans, ans);
+        }
+        return gans;
+    }
+};
