7babe86edcdbdb1e69ddf951be7af88f55f1d49c
Nikitiwe <rvnggm@gmail.com>
2024-08-11 22:02:04 +0700

    Time: 87 ms (39.42%), Space: 84.2 MB (34.62%) - LeetHub

--- 1721-maximum-profit-of-operating-a-centennial-wheel/1721-maximum-profit-of-operating-a-centennial-wheel.cpp
+++ 1721-maximum-profit-of-operating-a-centennial-wheel/1721-maximum-profit-of-operating-a-centennial-wheel.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int minOperationsMaxProfit(vector<int>& customers, int boardingCost, int runningCost) {
         if (runningCost>=4*boardingCost) return -1;
-        int ans=0, temp=0, count=0, i=0, id=0;
+        int ans=0, temp=0, count=0, i=0, id=0, t;
         while (i<customers.size()||count>0)
         {
             if (i<customers.size()) count+=customers[i];
-            temp+=min(4, count)*boardingCost;
-            count-=min(4, count);
+            t=min(4, count);
+            temp+=t*boardingCost;
+            count-=t;
             temp-=runningCost;
             i++;
             if (ans<temp) {id=i; ans=temp;}
