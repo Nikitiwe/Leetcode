8988ff79a42d0a528b5d609bee3a4f4aa7a13b30
Nikitiwe <rvnggm@gmail.com>
2024-08-14 16:53:08 +0700

    Time: 45 ms (88.09%), Space: 17.7 MB (75.86%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int ans = 0, tempr = 0;
-        bool isleftstay = 0;
-        for (int i = 0; i != directions.size(); i++)
+        int ans = 0, l = 0;
+        for (int i=0; i!=directions.size(); i++)
         {
-            if (directions[i]=='S')
-            {
-                isleftstay=1;
-                ans+=tempr;
-                tempr=0;
-            }
-            else if (directions[i]=='R') tempr++;
-            else
-            {
-                if (isleftstay==1||tempr>0) {ans++; isleftstay=1;}
-                ans+=tempr;
-                tempr=0;
-            }
+            if (directions[i]!='S') ans++;
         }
+        while (l<directions.size()&&directions[l]=='L') {ans--; l++;}
+        l=directions.size()-1;
+        while (l>=0&&directions[l]=='R') {ans--; l--;}
         return ans;
     }
 };
