4ccb4ab94a286389452fd01990e2f4ff9e231a17
Nikitiwe <rvnggm@gmail.com>
2024-08-25 17:10:48 +0700

    Time: 47 ms (19.12%), Space: 42 MB (14.34%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,7 +1,7 @@
 bool sort1(vector<int> v1, vector<int> v2)
 {
-    if (v1[0] != v2[0]) return v1[0] < v2[0];
-    else return v1[1] > v2[1];
+    if (v1[0] == v2[0]) return v1[1] > v2[1];
+    else return v1[0] < v2[0];
 }
 
 class Solution {
