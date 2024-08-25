8d886c5ee7b292ee0712b69b226361025a8b2a62
Nikitiwe <rvnggm@gmail.com>
2024-08-25 14:56:27 +0700

    Time: 89 ms (90.99%), Space: 111 MB (93.62%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -2,52 +2,60 @@ class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
         long long ans=1;
-        vector<vector<int>> arr(nums.size(), vector<int> (5, 1));
+        vector<int> next(5, 1), arr(5, 1);
         for (int i=1; i<nums.size(); i++)
         {
             if (nums[i]==nums[i-1]+2)
             {
-                arr[i][0]=1;
-                arr[i][1]=1;
-                arr[i][2]=arr[i-1][0]+1;
-                arr[i][3]=arr[i-1][0]+1;
-                arr[i][4]=arr[i-1][0]+1;
+                next[0]=1;
+                next[1]=1;
+                next[2]=arr[0]+1;
+                next[3]=arr[0]+1;
+                next[4]=arr[0]+1;
             }
             else if (nums[i]==nums[i-1]+1)
             {
-                arr[i][0]=1;
-                arr[i][1]=min(arr[i-1][1], arr[i-1][0])+1;
-                arr[i][2]=arr[i-1][1]+1;
-                arr[i][3]=arr[i-1][1]+1;
-                arr[i][4]=min(arr[i-1][1], arr[i-1][3])+1;
+                next[0]=1;
+                next[1]=min(arr[1], arr[0])+1;
+                next[2]=arr[1]+1;
+                next[3]=arr[1]+1;
+                next[4]=min(arr[1], arr[3])+1;
             }
             else if (nums[i]==nums[i-1])
             {
-                arr[i][0]=arr[i-1][0]+1;
-                arr[i][1]=arr[i-1][1]+1;
-                arr[i][2]=arr[i-1][2]+1;
-                arr[i][3]=arr[i-1][3]+1;
-                arr[i][4]=arr[i-1][4]+1;
+                next[0]=arr[0]+1;
+                next[1]=arr[1]+1;
+                next[2]=arr[2]+1;
+                next[3]=arr[3]+1;
+                next[4]=arr[4]+1;
             }
             else if (nums[i]==nums[i-1]-1)
             {
-                arr[i][0]=min(arr[i-1][3], arr[i-1][1])+1;
-                arr[i][1]=arr[i-1][3]+1;
-                arr[i][2]=arr[i-1][3]+1;
-                arr[i][3]=min(arr[i-1][3], arr[i-1][4])+1;
-                arr[i][4]=1;
+                next[0]=min(arr[3], arr[1])+1;
+                next[1]=arr[3]+1;
+                next[2]=arr[3]+1;
+                next[3]=min(arr[3], arr[4])+1;
+                next[4]=1;
             }
             else if (nums[i]==nums[i-1]-2)
             {
-                arr[i][0]=arr[i-1][4]+1;
-                arr[i][1]=arr[i-1][4]+1;
-                arr[i][2]=arr[i-1][4]+1;
-                arr[i][3]=1;
-                arr[i][4]=1;
+                next[0]=arr[4]+1;
+                next[1]=arr[4]+1;
+                next[2]=arr[4]+1;
+                next[3]=1;
+                next[4]=1;
             }
-            ans+=arr[i][2];
+            else
+            {
+                next[0]=1;
+                next[1]=1;
+                next[2]=1;
+                next[3]=1;
+                next[4]=1;
+            }
+            ans+=next[2];
+            arr=next;
         }
-        //return arr[2][0];
         return ans;
     }
 };
