59e7c53b87cca929dbb156909314eeafb2cc2605
Nikitiwe <rvnggm@gmail.com>
2024-08-29 02:38:50 +0700

    Time: 1536 ms (9.09%), Space: 220.6 MB (69.7%) - LeetHub

---
+++ 2241-recover-the-original-array/2241-recover-the-original-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> recoverArray(vector<int>& nums) {
+        unordered_map<int, int> m, arr, arr1;
+        sort(nums.begin(), nums.end());
+        for (int i=0; i!=nums.size(); i++) arr[nums[i]]++;
+        arr1=arr;
+        vector<int> ans;
+        for (int i=0; i!=nums.size(); i++)
+        {
+            for (int j=i+1; j<nums.size(); j++)
+            {
+                if (nums[j]-nums[i]>0&&(nums[j]-nums[i])%2==0) m[(nums[j]-nums[i])/2]++;
+            }
+        }
+        for (auto i=m.begin(); i!=m.end(); i++)
+        {
+            if (i->second>=nums.size()/2)
+            {
+                int k=i->first;
+                ans.clear();
+                arr=arr1;
+                for (int j=0; j!=nums.size(); j++)
+                {
+                    if (arr.count(nums[j])>0&&arr.count(nums[j]+2*k)>0)
+                    {
+                        ans.push_back(nums[j]+k);
+                        arr[nums[j]]--;
+                        if (arr[nums[j]]==0) arr.erase(nums[j]);
+                        arr[nums[j]+2*k]--;
+                        if (arr[nums[j]+2*k]==0) arr.erase(nums[j]+2*k);
+                    }
+                    //else break;
+                }
+                if (ans.size()==nums.size()/2) return ans;
+            }
+        }
+        return ans;
+    }
+};
