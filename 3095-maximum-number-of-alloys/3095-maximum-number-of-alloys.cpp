55eac3bd407043e916daf584f58f6dfe41164ece
Nikitiwe <rvnggm@gmail.com>
2024-08-31 15:34:55 +0700

    Time: 40 ms (67.37%), Space: 59.8 MB (43.22%) - LeetHub

--- 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.cpp
+++ 3095-maximum-number-of-alloys/3095-maximum-number-of-alloys.cpp
@@ -5,7 +5,7 @@ public:
         for (int i=0; i!=composition.size(); i++)
         {
             temp=0;
-            long l=0, r=2000000000, m=(l+r)/2, s=0;
+            long l=0, r=1000000000, m=(l+r)/2, s=0;
             while (l<r)
             {
                 s=0;
