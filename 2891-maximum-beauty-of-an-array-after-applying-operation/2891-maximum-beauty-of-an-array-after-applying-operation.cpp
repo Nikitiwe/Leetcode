1f19625e50c60b1414c0c9abfcd2dc64f3739fb3
Nikitiwe <rvnggm@gmail.com>
2024-08-31 23:53:27 +0700

    Time: 133 ms (94.52%), Space: 109.7 MB (33.97%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        vector<int> arr(*max_element(nums.begin(), nums.end())+1, 0);
+        int n=*min_element(nums.begin(), nums.end());
+        vector<int> arr(*max_element(nums.begin(), nums.end())-n+1, 0);
         for (int i=0; i!=nums.size(); i++)
         {
-            if (nums[i]-k<0) arr[0]++;
-            else arr[nums[i]-k]++;
-            if (nums[i]+k+1>arr.size()-1) ;
-            else arr[nums[i]+k+1]--;
+            if (nums[i]-k<n) arr[0]++;
+            else arr[nums[i]-k-n]++;
+            if (nums[i]+k+1>arr.size()-1+n) ;
+            else arr[nums[i]+k+1-n]--;
         }
         for (int i=0; i<arr.size()-1; i++) arr[i+1]+=arr[i];
         return *max_element(arr.begin(), arr.end());
