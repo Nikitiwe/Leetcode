6df8189938f942820f145c0462c175b2c1515617
Nikitiwe <rvnggm@gmail.com>
2024-09-02 02:23:37 +0700

    Time: 73 ms (85.77%), Space: 87.9 MB (83.87%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -3,11 +3,13 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& og, int m, int n) {
         if (m*n!=og.size()) return {};
         vector<vector<int>> ans (m, vector<int> (n, 0));
+        int t;
         for (int i=0; i!=m; i++)
         {
+            t=i*n;
             for (int j=0; j!=n; j++)
             {
-                ans[i][j]=og[i*n+j];
+                ans[i][j]=og[t+j];
             }
         }
         return ans;
