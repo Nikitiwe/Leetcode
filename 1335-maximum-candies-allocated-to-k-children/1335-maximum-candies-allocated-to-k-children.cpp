1715c90c7ffff708794a0a8ec3e868fd097d152d
Nikitiwe <rvnggm@gmail.com>
2024-09-02 01:15:12 +0700

    Time: 144 ms (30.72%), Space: 87.1 MB (27.09%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int maximumCandies(vector<int>& can, long long k) {
-        long long l=0, r=10000000, m=(l+r)/2, ans=0, temp;
+        long long l=0, r=*max_element(can.begin(), can.end()), m=(l+r)/2, ans=0, temp;
         while (l<r)
         {
+            if (m==0) m++;
             temp=0;
             for (int i=0; i!=can.size(); i++)
             {
@@ -12,6 +13,7 @@ public:
             if (temp<k) {r=m-1;}
             else {l=m; r++;}
             m=(l+r)/2;
+            //if (m==0) return 0;
         }
         return m;
     }
