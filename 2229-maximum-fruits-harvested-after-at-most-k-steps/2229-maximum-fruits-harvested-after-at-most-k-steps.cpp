2cf3f3c91b4ed50c936e2fa3275747cba8171ef7
Nikitiwe <rvnggm@gmail.com>
2024-09-06 02:45:26 +0700

    Time: 293 ms (47.73%), Space: 155.8 MB (47.27%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
         vector<int> arr(max(startPos, fruits[fruits.size()-1][0])+1, 0);
-        int t=0, l=0, ans=0, tt=0;
+        int t=0, l=0, ans=0, tt=0, c;
         for (int i=0; i<fruits.size(); i++) if (fruits[i][0]==startPos) {tt=fruits[i][1]; break;}
         for (int i=startPos+1; i<arr.size(); i++)
         {
@@ -17,25 +17,27 @@ public:
             if (l>=0&&fruits[l][0]==i) t+=fruits[l][1];
             arr[i]=t;
         }
+        c=startPos+k;
         for (int i=1; i<=k; i++)
         {
             t=0;
             if (startPos>=i)
             {
                 t=arr[startPos-i];
-                if (startPos-i+(k-i)<arr.size()&&k>2*i) t+=arr[startPos-i+(k-i)];
-                else if (startPos-i+(k-i)>=arr.size()) t+=arr[arr.size()-1];
+                if (c-2*i<arr.size()&&k>2*i) t+=arr[c-2*i];
+                else if (c-2*i>=arr.size()) t+=arr[arr.size()-1];
             }
             ans=max(ans, t);
         }
+        c=startPos-k;
         for (int i=1; i<=k; i++)
         {
             t=0;
             if (startPos+i<arr.size())
             {
                 t=arr[startPos+i];
-                if (startPos+i-(k-i)>=0&&2*i<k) t+=arr[startPos+i-(k-i)];
-                else if (startPos+i-(k-i)<0) t+=arr[0];
+                if (c+2*i>=0&&2*i<k) t+=arr[c+2*i];
+                else if (c+2*i<0) t+=arr[0];
             }
             ans=max(ans, t);
         }
