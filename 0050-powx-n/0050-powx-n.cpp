e26d4ea6d19b43a75c8988823700b2aca6fcd120
Nikitiwe <rvnggm@gmail.com>
2024-09-07 22:12:13 +0700

    Time: 3 ms (39.2%), Space: 8.3 MB (42.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     double myPow(double x, int nn) {
+        if (nn==0) return 1;
         double ans=1, temp=1;
         long n=nn;
-        int m=10000;
-        if (n==0) return ans;
         if (n<0)
         {
             n*=-1;
             x=1/x;
         }
-        if (n>10000)
+        if (n>0)
         {
-            while (m>0)
-            {
-                temp*=x;
-                m--;
-            }
-            if (temp==0.00000) return 0.00000;
-            //if (temp==1.00000) return 1.00000;
-        }
-        while (n>10000)
-        {
-            ans*=temp;
-            n-=10000;
+            temp=x;
+            //n--;
         }
         while (n>0)
         {
-            ans*=x;
-            n--;
+            if (n%2==0)
+            {
+                temp*=temp;
+                n/=2;
+            }
+            else
+            {
+                ans*=temp;
+                temp*=temp;
+                n/=2;
+            }
         }
         return ans;
     }
