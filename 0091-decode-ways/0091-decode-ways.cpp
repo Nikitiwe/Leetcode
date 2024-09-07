5f62efc04318c5e753056ae3a7cdaa8a77e5fe52
Nikitiwe <rvnggm@gmail.com>
2024-09-08 00:36:32 +0700

    Time: 0 ms (100%), Space: 7.9 MB (92.5%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> arr(s.size(), 0);
+        int a=0, b=0, t=0;
         if (s[0]=='0') return 0;
-        else arr[0]=1;
+        else a=1;
         if (s.size()==1) return 1;
-        if ((s[0]-'0')*10+(s[1]-'0')<=26) arr[1]=1;
-        if (s[1]!='0') arr[1]+=arr[0];
+        if ((s[0]-'0')*10+(s[1]-'0')<=26) b=1;
+        if (s[1]!='0') b+=a;
         for (int i=2; i<s.size(); i++)
         {
-            if (s[i]!='0') arr[i]+=arr[i-1];
-            if (s[i-1]!='0'&&(s[i-1]-'0')*10+(s[i]-'0')<=26) arr[i]+=arr[i-2];
-            if (arr[i]==0) return 0;
+            t=0;
+            if (s[i]!='0') t+=b;
+            if (s[i-1]!='0'&&(s[i-1]-'0')*10+(s[i]-'0')<=26) t+=a;
+            if (t==0) return 0;
+            a=b;
+            b=t;
         }
-        return arr[arr.size()-1];
+        return b;
     }
 };
