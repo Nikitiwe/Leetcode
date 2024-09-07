544fcbd8fa3450ee0dffc19d5c88fd88ee7f810f
Nikitiwe <rvnggm@gmail.com>
2024-09-07 22:37:11 +0700

    Time: 55 ms (58.78%), Space: 19.4 MB (62.24%) - LeetHub

--- 1647-can-convert-string-in-k-moves/1647-can-convert-string-in-k-moves.cpp
+++ 1647-can-convert-string-in-k-moves/1647-can-convert-string-in-k-moves.cpp
@@ -3,12 +3,12 @@ public:
     bool canConvertString(string s, string t, int k) {
         if (s.size()!=t.size()) return 0;
         vector<int> arr(26, 0);
+        int a=k/26;
         for (int i=0; i!=s.size(); i++) arr[((t[i]-s[i])%26+26)%26]++;
         for (int i=1; i!=26; i++)
         {
-            int a=k/26;
-            if (k%26>=i) a++;
-            if (a<arr[i]) return 0;
+            if (k%26>=i) {if (a+1<arr[i]) return 0;}
+            else if (a<arr[i]) return 0;
         }
         return 1;
     }
