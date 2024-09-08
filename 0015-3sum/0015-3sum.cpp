0abed7b85c2a837e94f89401f5d85a5c3682c71e
Nikitiwe <rvnggm@gmail.com>
2024-09-09 00:54:20 +0700

    Time: 786 ms (20.04%), Space: 32.4 MB (28.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,6 +8,7 @@ public:
         unordered_set<long> n;
         for (int i=0; i!=nums.size(); i++)
         {
+            if (nums[i]>0) break;
             for (int j=i+1; j<nums.size(); j++)
             {
                 if (m.count(nums[i]+nums[j])>0&&m[nums[i]+nums[j]]>j)
