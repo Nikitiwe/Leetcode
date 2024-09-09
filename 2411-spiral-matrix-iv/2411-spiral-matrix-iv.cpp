6ca9386fdd75edb57a235edc112ee5387b7d7596
Nikitiwe <rvnggm@gmail.com>
2024-09-09 14:15:09 +0700

    Time: 147 ms (92.63%), Space: 138 MB (5.11%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -11,49 +11,45 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<int> arr(n*m, -1);
+        vector<int> arr;
         ListNode* s=head;
         int i=0;
         while (s!=nullptr)
         {
-            arr[i]=s->val;
+            arr.push_back(s->val);
             i++;
             s=s->next;
         }
         i=0;
         int x=0, y=0;
-        vector<vector<int>> ans(m, vector<int> (n, -2));
+        vector<vector<int>> ans(m, vector<int> (n, -1));
         ans[y][x]=arr[i];
         while (i<arr.size()-1)
         {
-            while (x+1<n&&ans[y][x+1]==-2)
+            while (x+1<n&&ans[y][x+1]==-1&&i<arr.size()-1)
             {
                 x++;
                 i++;
                 ans[y][x]=arr[i];
             }
-            //if (i==arr.size()) break;
-            while (y+1<m&&ans[y+1][x]==-2)
+            while (y+1<m&&ans[y+1][x]==-1&&i<arr.size()-1)
             {
                 y++;
                 i++;
                 ans[y][x]=arr[i];
             }
-            //if (i==arr.size()) break;
-            while (x-1>=0&&ans[y][x-1]==-2)
+            while (x-1>=0&&ans[y][x-1]==-1&&i<arr.size()-1)
             {
                 x--;
                 i++;
                 ans[y][x]=arr[i];
             }
-            //if (i==arr.size()) break;
-            while (y-1>=0&&ans[y-1][x]==-2)
+            while (y-1>=0&&ans[y-1][x]==-1&&i<arr.size()-1)
             {
                 y--;
                 i++;
                 ans[y][x]=arr[i];
             }
-            //if (i==arr.size()) break;
         }
         return ans;
     }
