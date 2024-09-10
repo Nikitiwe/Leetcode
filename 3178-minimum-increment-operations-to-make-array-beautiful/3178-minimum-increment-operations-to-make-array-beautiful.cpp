e54d2000583d8524b2def52b810ee5e5b74cf3d9
Nikitiwe <rvnggm@gmail.com>
2024-09-10 12:25:39 +0700

    Time: 86 ms (79.59%), Space: 89.8 MB (70.75%) - LeetHub

--- 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     long long minIncrementOperations(vector<int>& nums, int k) {
-        vector<long long> arr(nums.size(), 0);
         vector<long long> ans(nums.size(), 0);
-        for (int i=0; i<nums.size(); i++)
-        {
-            if (nums[i]<k) arr[i]=k-nums[i];
-        }
-        ans[0]=arr[0];
-        ans[1]=arr[1];
-        ans[2]=arr[2];
+        if (nums[0]<k) ans[0]=k-nums[0];
+        if (nums[1]<k) ans[1]=k-nums[1];
+        if (nums[2]<k) ans[2]=k-nums[2];
         for (int i=3; i<nums.size(); i++)
         {
-            ans[i]=arr[i]+min({ans[i-1], ans[i-2], ans[i-3]});
+            ans[i]=+min({ans[i-1], ans[i-2], ans[i-3]});
+            if (nums[i]<k) ans[i]+=k-nums[i];
         }
         return min({ans[nums.size()-1], ans[nums.size()-2], ans[nums.size()-3]});
     }
