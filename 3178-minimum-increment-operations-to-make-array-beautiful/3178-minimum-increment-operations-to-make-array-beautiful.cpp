61f5090d67b92364e828cea49b7ca929496eb431
Nikitiwe <rvnggm@gmail.com>
2024-09-10 12:23:04 +0700

    Time: 99 ms (51.36%), Space: 95.6 MB (44.22%) - LeetHub

--- 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.cpp
+++ 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     long long minIncrementOperations(vector<int>& nums, int k) {
         vector<long long> arr(nums.size(), 0);
-        vector<vector<long long>> ans(nums.size(), vector<long long> (3, 0));
+        vector<long long> ans(nums.size(), 0);
         for (int i=0; i<nums.size(); i++)
         {
             if (nums[i]<k) arr[i]=k-nums[i];
         }
-        ans[0][0]=arr[0]; ans[0][1]=9999999; ans[0][2]=9999999;
-        ans[1][0]=arr[1]; ans[1][1]=arr[0]; ans[1][2]=9999999;
-        ans[2][0]=arr[2]; ans[2][1]=arr[1]; ans[2][2]=arr[0];
+        ans[0]=arr[0];
+        ans[1]=arr[1];
+        ans[2]=arr[2];
         for (int i=3; i<nums.size(); i++)
         {
-            ans[i][0]=arr[i]+min({ans[i-1][0], ans[i-1][1], ans[i-1][2]});
-            ans[i][1]=ans[i-1][0];
-            ans[i][2]=ans[i-2][0];
+            ans[i]=arr[i]+min({ans[i-1], ans[i-2], ans[i-3]});
         }
-        return min({ans[nums.size()-1][0], ans[nums.size()-1][1], ans[nums.size()-1][2]});
+        return min({ans[nums.size()-1], ans[nums.size()-2], ans[nums.size()-3]});
     }
 };
