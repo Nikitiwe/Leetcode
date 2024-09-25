0993dc38822a3dafa07d796da825c5299fbd124f
Nikitiwe <rvnggm@gmail.com>
2024-09-25 17:41:20 +0700

    Time: 98 ms (63.37%), Space: 88.8 MB (44.32%) - LeetHub

--- 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
+++ 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
@@ -22,8 +22,8 @@ public:
             if (i>0) ma[i]=max(ma[i], ma[i-1]);
             if (i>0&&mi[i-1]>-1) mi[i]=min(mi[i], mi[i-1]);
 
-            if (ma[i]>-1&&ma[i]-nums[i]>=vd) return {i, (int)(find(nums.begin(), nums.end(), ma[i])-nums.begin())};
-            if (mi[i]>-1&&nums[i]-mi[i]>=vd) return {i, (int)(find(nums.begin(), nums.end(), mi[i])-nums.begin())};
+            if (ma[i]-nums[i]>=vd) return {i, (int)(find(nums.begin(), nums.end(), ma[i])-nums.begin())};
+            if (nums[i]-mi[i]>=vd) return {i, (int)(find(nums.begin(), nums.end(), mi[i])-nums.begin())};
         }
         return {-1, -1};
     }
