6e481aa4df455ff3b5591c0e1e4e4a3d8fd07724
Nikitiwe <rvnggm@gmail.com>
2024-10-06 05:12:29 +0700

    Time: 41 ms (8.09%), Space: 76.6 MB (42.2%) - LeetHub

---
+++ 2684-determine-the-winner-of-a-bowling-game/2684-determine-the-winner-of-a-bowling-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int isWinner(vector<int>& p1, vector<int>& p2) {
+        int sum1=0, sum2=0;
+        for (int i=0; i!=p1.size(); i++)
+        {
+            if ((i>0&&p1[i-1]==10)||(i>1&&p1[i-2]==10)) sum1+=2*p1[i];
+            else sum1+=p1[i];
+        }
+        for (int i=0; i!=p2.size(); i++)
+        {
+            if ((i>0&&p2[i-1]==10)||(i>1&&p2[i-2]==10)) sum2+=2*p2[i];
+            else sum2+=p2[i];
+        }
+        if (sum1>sum2) return 1;
+        else if (sum1<sum2) return 2;
+        else return 0;
+    }
+};
