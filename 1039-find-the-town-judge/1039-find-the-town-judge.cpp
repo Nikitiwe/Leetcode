60004d5dc6270a2f029c7ff23d37e586a6fc97fd
Nikitiwe <rvnggm@gmail.com>
2024-10-07 18:57:54 +0700

    Time: 126 ms (48.84%), Space: 65.3 MB (53.81%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.cpp
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -5,12 +5,13 @@ public:
         unordered_map<int, int> m;
         for (int i=0; i!=t.size(); i++)
         {
+            m[t[i][0]]=-2000;
             m[t[i][1]]++;
         }
-        for (int i=0; i!=t.size(); i++)
+        /*for (int i=0; i!=t.size(); i++)
         {
             m.erase(t[i][0]);
-        }
+        }*/
         for (auto i=m.begin(); i!=m.end(); i++)
         {
             if (i->second==n-1) return i->first;
