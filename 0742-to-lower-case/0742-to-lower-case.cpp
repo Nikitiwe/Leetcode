1fd267265e9b5e5aebb15693fca2b99ae4b2d980
Nikitiwe <rvnggm@gmail.com>
2024-10-07 17:45:51 +0700

    Time: 0 ms (100%), Space: 7.7 MB (62.61%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cpp
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for (int i=0; i!=s.size(); i++) s[i]=tolower(s[i]);
+        for (int i=0; i!=s.size(); i++)
+        {
+            // s[i]=tolower(s[i]);
+            if (s[i]>='A'&&s[i]<='Z') s[i]+='a'-'A';
+        }
         return s;
     }
 };
