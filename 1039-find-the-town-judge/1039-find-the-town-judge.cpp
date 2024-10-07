1f3469cfb2d80b40467428b93a0beda79e2c1108
Nikitiwe <rvnggm@gmail.com>
2024-10-07 18:56:10 +0700

    Time: 127 ms (47.55%), Space: 65.2 MB (54.29%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& t) {
+        if (n==1) return 1;
+        unordered_map<int, int> m;
+        for (int i=0; i!=t.size(); i++)
+        {
+            m[t[i][1]]++;
+        }
+        for (int i=0; i!=t.size(); i++)
+        {
+            m.erase(t[i][0]);
+        }
+        for (auto i=m.begin(); i!=m.end(); i++)
+        {
+            if (i->second==n-1) return i->first;
+        }
+        return -1;
+    }
+};
