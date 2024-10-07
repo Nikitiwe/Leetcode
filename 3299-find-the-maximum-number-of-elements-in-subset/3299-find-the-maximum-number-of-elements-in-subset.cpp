5ccb0e3ba05ebf78b10999d275dc725049a04ba4
Nikitiwe <rvnggm@gmail.com>
2024-10-08 02:06:58 +0700

    Time: 193 ms (94.29%), Space: 118.9 MB (92.47%) - LeetHub

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/3299-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int ans=1;
+        unordered_map<int, int> m;
+        for (int i=0; i!=nums.size(); i++) m[nums[i]]++;
+        for (auto i=m.begin(); i!=m.end(); i++)
+        {
+            if (i->first==1)
+            {
+                if (i->second%2==1) ans=max(ans, i->second);
+                else ans=max(ans, i->second-1);
+            }
+            else if (i->second>1)
+            {
+                int count=1;
+                long t=(long)i->first*i->first;
+                while (m.count(t)>0)
+                {
+                    ans=max(ans, count*2+1);
+                    if (m[t]==1||t>100000) break;
+                    t*=t;
+                    count++;
+                }
+            }
+        }
+        return ans;
+    }
+};
