af481e8e1b2f6e4e3c9f266ff81a4c2996fe06b8
Nikitiwe <rvnggm@gmail.com>
2024-10-08 21:21:08 +0700

    Time: 2081 ms (19.27%), Space: 433.2 MB (25.69%) - LeetHub

--- 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int maximizeSquareArea(int a, int b, vector<int>& h, vector<int>& v) {
-        unordered_set<int> m, n;
+        unordered_map<int, int> m;
         h.push_back(1);
         h.push_back(a);
         for (int i=0; i!=h.size(); i++)
         {
             for (int j=0; j<i; j++)
             {
-                m.insert(abs(h[i]-h[j]));
+                m[abs(h[i]-h[j])]=1;
             }
         }
+        int ans=-1, t;
         v.push_back(1);
         v.push_back(b);
         for (int i=0; i!=v.size(); i++)
         {
             for (int j=0; j<i; j++)
             {
-                n.insert(abs(v[i]-v[j]));
+                t=abs(v[i]-v[j]);
+                if (m[t]==1) m[t]=2;
+                if (m[t]>1) ans=max(ans, t);
             }
-        }
-        int ans=-1;
-        for (auto i=m.begin(); i!=m.end(); i++)
-        {
-            if (n.count(*i)>0) ans=max(ans, *i);
         }
         if (ans==-1) return ans;
         else return (long long)ans*ans%1000000007;
