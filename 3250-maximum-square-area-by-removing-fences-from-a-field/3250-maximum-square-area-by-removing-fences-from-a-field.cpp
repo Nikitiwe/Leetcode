33d2347a6cf4e0a2636e1b52d8097dfc07f686a8
Nikitiwe <rvnggm@gmail.com>
2024-10-08 21:22:45 +0700

    Time: 2071 ms (19.27%), Space: 433.3 MB (24.77%) - LeetHub

--- 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/3250-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -4,21 +4,23 @@ public:
         unordered_map<int, int> m;
         h.push_back(1);
         h.push_back(a);
+        sort(h.begin(), h.end());
         for (int i=0; i!=h.size(); i++)
         {
             for (int j=0; j<i; j++)
             {
-                m[abs(h[i]-h[j])]=1;
+                m[h[i]-h[j]]=1;
             }
         }
         int ans=-1, t;
         v.push_back(1);
         v.push_back(b);
+        sort(v.begin(), v.end());
         for (int i=0; i!=v.size(); i++)
         {
             for (int j=0; j<i; j++)
             {
-                t=abs(v[i]-v[j]);
+                t=v[i]-v[j];
                 if (m[t]==1) m[t]=2;
                 if (m[t]>1) ans=max(ans, t);
             }
