aace5dd301fbfecc873bf11b6d49b729d43fcdb0
Nikitiwe <rvnggm@gmail.com>
2024-10-09 04:26:43 +0700

    Time: 5 ms (27.3%), Space: 8.1 MB (71.58%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+        int t=0;
+        for (int i=s.size()-1; i>=0; i--)
+        {
+            if (s[i]>='0'&&s[i]<='9')
+            {
+                t++;
+            }
+            else
+            {
+                if (t>0) t--;
+                else ans+=s[i];
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
