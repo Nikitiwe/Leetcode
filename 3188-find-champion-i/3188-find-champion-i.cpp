e8b9bafcc90338c59e9ae9443fb14d2619a72a5d
Nikitiwe <rvnggm@gmail.com>
2024-10-14 01:16:13 +0700

    Time: 43 ms (93.56%), Space: 40.5 MB (29.58%) - LeetHub

--- 3188-find-champion-i/3188-find-champion-i.cpp
+++ 3188-find-champion-i/3188-find-champion-i.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) {
+        vector<bool> arr(grid.size(), 1);
         for (int i=0; i!=grid.size(); i++)
         {
-            int sum=0;
-            for (int j=0; j!=grid[0].size(); j++)
+            if (arr[i]==1)
             {
-                sum+=grid[i][j];
+                int sum=0;
+                for (int j=0; j!=grid[0].size(); j++)
+                {
+                    if (grid[i][j]==1)
+                    {
+                        sum++;
+                        arr[j]=0;
+                    }
+                }
+                if (sum==grid[0].size()-1) return i;
             }
-            if (sum==grid[0].size()-1) return i;
         }
         return -1;
     }
