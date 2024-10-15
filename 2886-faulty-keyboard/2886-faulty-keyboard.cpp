de92774fa79a23f95030445de9f4f153ed4eb72e
Nikitiwe <rvnggm@gmail.com>
2024-10-16 04:33:39 +0700

    Time: 9 ms (29.96%), Space: 10.1 MB (22.79%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     string finalString(string s) {
-        string ans;
+        string r, l;
+        int c=0;
+        bool d=1;
         for (int i=0; i!=s.size(); i++)
         {
-            if (s[i]=='i') reverse(ans.begin(), ans.end());
-            else ans+=s[i];
+            if (s[i]=='i')
+            {
+                c++;
+                if (d==1) d=0;
+                else d=1;
+            }
+            else if (d==1) r+=s[i];
+            else l+=s[i];
         }
-        return ans;
+        reverse(l.begin(), l.end());
+        l+=r;
+        if (c%2==0) return l;
+        else
+        {
+            reverse(l.begin(), l.end());
+            return l;
+        }
     }
 };
