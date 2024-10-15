66d231df343403b356b6e24f0697fa5336c0c569
Nikitiwe <rvnggm@gmail.com>
2024-10-16 05:11:10 +0700

    Time: 12 ms (86.58%), Space: 70.5 MB (69.01%) - LeetHub

--- 2870-longest-alternating-subarray/2870-longest-alternating-subarray.cpp
+++ 2870-longest-alternating-subarray/2870-longest-alternating-subarray.cpp
@@ -4,20 +4,21 @@ public:
         int ans=-1;
         for (int i=0; i!=nums.size(); i++)
         {
-            int t=nums[i];
+            int t=nums[i], add=0;
             for (int j=1; j<nums.size()-i; j++)
             {
                 if (j%2==1)
                 {
-                    if (nums[i+j]==t+1) ans=max(ans, j+1);
+                    if (nums[i+j]==t+1) {ans=max(ans, j+1); add++;}
                     else break;
                 }
                 else
                 {
-                    if (nums[i+j]==t) ans=max(ans, j+1);
+                    if (nums[i+j]==t) {ans=max(ans, j+1); add++;}
                     else break;
                 }
             }
+            if (add>0) i+=add-1;
         }
         return ans;
     }
