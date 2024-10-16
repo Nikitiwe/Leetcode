d9f7c3c3962c353beb19c1bd158182fe4c678b9f
Nikitiwe <rvnggm@gmail.com>
2024-10-16 16:36:28 +0700

    Time: 72 ms (86.42%), Space: 97.8 MB (56.75%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
         if (nums.size()<=2) return -1;
-        sort(nums.begin(), nums.begin()+3);
+        if (nums[0]>nums[1]) swap(nums[0], nums[1]);
+        if (nums[1]>nums[2]) swap(nums[1], nums[2]);
+        if (nums[0]>nums[1]) swap(nums[0], nums[1]);
         return nums[1];
     }
 };
