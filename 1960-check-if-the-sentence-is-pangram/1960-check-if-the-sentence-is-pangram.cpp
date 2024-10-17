ab32509f79e1a9135696d0aafa1f2cadc3beaa2f
Nikitiwe <rvnggm@gmail.com>
2024-10-18 04:12:48 +0700

    Time: 0 ms (100%), Space: 8.1 MB (32.19%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<int> arr(26, 0);
+        for (int i=0; i!=s.size(); i++)
+        {
+            arr[s[i]-'a']++;
+        }
+        for (int i=0; i!=26; i++)
+        {
+            if (arr[i]==0) return 0;
+        }
+        return 1;
+    }
+};
