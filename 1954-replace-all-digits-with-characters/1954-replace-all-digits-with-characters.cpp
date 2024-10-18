25d3c2b472e4edbe785663d43391eb38b731e62d
Nikitiwe <rvnggm@gmail.com>
2024-10-18 17:14:45 +0700

    Time: 0 ms (100%), Space: 7.7 MB (7.26%) - LeetHub

--- 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.cpp
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.cpp
@@ -3,7 +3,7 @@ public:
     string replaceDigits(string s) {
         for (int i=1; i<s.size(); i+=2)
         {
-            s[i]=s[i-1]+s[i]-'0';
+            s[i]+=s[i-1]-'0';
         }
         return s;
     }
