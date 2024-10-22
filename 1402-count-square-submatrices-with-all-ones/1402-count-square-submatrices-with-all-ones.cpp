4d6888ed264401a825f0ddd02a1e862c03a48e68
Nikitiwe <rvnggm@gmail.com>
2024-10-22 17:14:40 +0700

    Time: 4 ms (98.68%), Space: 26.4 MB (93.05%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& m) {
         int ans=0;
-        vector<vector<int>> next=m;
-        for (int i=0; i!=m.size(); i++)
-        {
-            for (int j=0; j!=m[0].size(); j++)
+
+        for (int i=0; i!=m.size(); i++) ans+=m[i][0];
+        for (int j=1; j<m[0].size(); j++) ans+=m[0][j];
+
+            for (int i=1; i<m.size(); i++)
             {
-                ans+=m[i][j];
-            }
-        }
-        for (int k=2; k<max(m.size(), m[0].size()); k++)
-        {
-            for (int i=k-1; i<m.size(); i++)
-            {
-                for (int j=k-1; j<m[0].size(); j++)
+                for (int j=1; j<m[0].size(); j++)
                 {
-                    if (m[i][j]>0&&m[i-1][j-1]==k-1&&m[i][j-1]==k-1&&m[i-1][j]==k-1)
+                    if (m[i][j]>0)
                     {
-                        ans++;
-                        next[i][j]=k;
+                        m[i][j]=min({m[i-1][j-1], m[i][j-1], m[i-1][j]})+1;
+                        ans+=m[i][j];
                     }
                 }
             }
-            m=next;
-        }
+
         return ans;
     }
 };
