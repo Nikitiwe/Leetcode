0bfa7ba82be41a2ad233d3404a161c4641b30418
Nikitiwe <rvnggm@gmail.com>
2024-10-25 05:34:12 +0700

    Time: 399 ms (6.03%), Space: 236.1 MB (5.23%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> m, next;
+        vector<int> tt(1, 0);
         for (int i=1; i<=n; i++)
         {
-            vector<int> t;
-            t.push_back(i);
-            m.push_back(t);
+            tt[0]=i;
+            m.push_back(tt);
         }
         for (int i=2; i<=k; i++)
         {
             for (int j=0; j!=m.size(); j++)
             {
-                for (int a=m[j][m[j].size()-1]+1; a<=n; a++)
+                vector<int> t=m[j];
+                t.push_back(0);
+                for (int a=t[i-2]+1; a<=n; a++)
                 {
-                    vector<int> t=m[j];
-                    t.push_back(a);
+                    t[i-1]=a;
                     next.push_back(t);
                 }
             }
