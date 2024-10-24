da3739acaeea181bbe678edc8e7f86616122c83e
Nikitiwe <rvnggm@gmail.com>
2024-10-25 01:42:33 +0700

    Time: 2 ms (99.42%), Space: 16.8 MB (86.87%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
@@ -3,9 +3,9 @@ public:
     int similarPairs(vector<string>& w) {
         int ans=0;
         unordered_map<int, int> m;
+        vector<int> arr(26, 0), n=arr;
         for (int i=0; i!=w.size(); i++)
         {
-            vector<int> arr(26, 0);
             for (int j=0; j!=w[i].size(); j++) arr[w[i][j]-'a']=max(arr[w[i][j]-'a'], 1);
             int t=0, d=1;
             for (int k=0; k!=26; k++)
@@ -15,6 +15,7 @@ public:
             }
             m[t]++;
             ans+=m[t]-1;
+            arr=n;
         }
         return ans;
     }
