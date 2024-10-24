0c82b7ef650dd3d3a07356653295c7c76b7dcac9
Nikitiwe <rvnggm@gmail.com>
2024-10-25 01:41:12 +0700

    Time: 9 ms (95.29%), Space: 17.9 MB (71.84%) - LeetHub

--- 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
+++ 2594-count-pairs-of-similar-strings/2594-count-pairs-of-similar-strings.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int similarPairs(vector<string>& w) {
-        vector<vector<int>> arr(w.size(), vector<int> (26, 0));
-        for (int i=0; i!=w.size(); i++)
-        {
-            for (int j=0; j!=w[i].size(); j++) arr[i][w[i][j]-'a']++;
-        }
         int ans=0;
+        unordered_map<int, int> m;
         for (int i=0; i!=w.size(); i++)
         {
-            for (int j=i+1; j<w.size(); j++)
+            vector<int> arr(26, 0);
+            for (int j=0; j!=w[i].size(); j++) arr[w[i][j]-'a']=max(arr[w[i][j]-'a'], 1);
+            int t=0, d=1;
+            for (int k=0; k!=26; k++)
             {
-                bool isit=1;
-                for (int k=0; k!=26; k++)
-                {
-                    if ((arr[i][k]==0&&arr[j][k]!=0)||(arr[i][k]!=0&&arr[j][k]==0))
-                    {
-                        isit=0;
-                        break;
-                    }
-                }
-                if (isit==1) ans++;
+                t+=d*arr[k];
+                d*=2;
             }
+            m[t]++;
+            ans+=m[t]-1;
         }
         return ans;
     }
