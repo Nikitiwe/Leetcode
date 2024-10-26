1d7e799266438172191da9866d94e7e3a95f8bb2
Nikitiwe <rvnggm@gmail.com>
2024-10-27 00:42:26 +0700

    Time: 11 ms (97.89%), Space: 33.9 MB (44.76%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& w, int d) {
+        int l=1, r=30000000, m;
+        while (l<r-1)
+        {
+            m=(l+r)/2;
+            int s=1, t=0;
+            for (int i=0; i!=w.size(); i++)
+            {
+                if (t+w[i]<=m) t+=w[i];
+                else
+                {
+                    s++;
+                    t=w[i];
+                    if (t>m)
+                    {
+                        s=100000;
+                        break;
+                    }
+                }
+            }
+            if (s>d) l=m+1;
+            else r=m;
+        }
+
+        int s=1, t=0, n=m-1;
+        {
+            for (int i=0; i!=w.size(); i++)
+            {
+                if (t+w[i]<=n) t+=w[i];
+                else
+                {
+                    s++;
+                    t=w[i];
+                    if (t>n)
+                    {
+                        s=100000;
+                        break;
+                    }
+                }
+            }
+        }
+        if (s<=d) return n;
+        else
+        {
+        s=1; t=0;
+        {
+            for (int i=0; i!=w.size(); i++)
+            {
+                if (t+w[i]<=m) t+=w[i];
+                else
+                {
+                    s++;
+                    t=w[i];
+                    if (t>m)
+                    {
+                        s=100000;
+                        break;
+                    }
+                }
+            }
+        }
+        if (s<=d) return m;
+        else
+        {
+            int mm=m+1;
+            s=1; t=0;
+            for (int i=0; i!=w.size(); i++)
+            {
+                if (t+w[i]<=mm) t+=w[i];
+                else
+                {
+                    s++;
+                    t=w[i];
+                    if (t>mm)
+                    {
+                        s=100000;
+                        break;
+                    }
+                }
+            }
+            if (s<=d) return mm;
+            else return mm+1;
+        }
+        }
+        return -1;
+    }
+};
