a8c1d24301c46ae73c9dd3ee959728514d689ece
Nikitiwe <rvnggm@gmail.com>
2024-10-28 18:52:22 +0700

    Time: 172 ms (13.33%), Space: 136.6 MB (9.33%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -12,6 +12,7 @@ public:
             if (m.count(nums[i])>0) m[(long)nums[i]*nums[i]]=m[nums[i]]+1;
             else m[(long)nums[i]*nums[i]]=1;
             if (m[(long)nums[i]*nums[i]]>1) ans=max(ans, m[(long)nums[i]*nums[i]]);
+            if (ans==5) return ans;
         }
         return ans;
     }
