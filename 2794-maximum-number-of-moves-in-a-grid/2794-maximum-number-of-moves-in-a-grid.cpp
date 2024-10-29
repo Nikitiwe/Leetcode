c923139ed0d71506f49f88bba44c11b8dd72b46f
Nikitiwe <rvnggm@gmail.com>
2024-10-29 13:50:16 +0700

    Time: 244 ms (6.52%), Space: 74.4 MB (57.28%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -3,9 +3,9 @@ public:
     int maxMoves(vector<vector<int>>& grid) {
         vector<vector<int>> ans(grid.size(), vector<int> (grid[0].size(), 0));
         int temp=0, m=0;
+        for (int k=0; k<grid.size(); k++)  ans[k][0]=1;
         for (int k=0; k<grid.size(); k++)
         {
-            ans[k][0]=1;
             for (int j=1; j<grid[0].size(); j++)
             {
                 for (int i=0; i<grid.size(); i++)
