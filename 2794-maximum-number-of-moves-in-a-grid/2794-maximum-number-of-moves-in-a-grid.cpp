47127105541f6ad429b00c0f51c1cea63b0f8ae3
Nikitiwe <rvnggm@gmail.com>
2024-10-29 13:48:10 +0700

    Time: 74 ms (19.57%), Space: 74.4 MB (43.37%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
-        vector<vector<int>> ans(grid.size(), vector<int> (grid[0].size(), 0)), zero=ans;
+        vector<vector<int>> ans(grid.size(), vector<int> (grid[0].size(), 0));
         int temp=0, m=0;
         for (int k=0; k<grid.size(); k++)
         {
@@ -10,15 +10,18 @@ public:
             {
                 for (int i=0; i<grid.size(); i++)
                 {
-                    if (i>0&&ans[i-1][j-1]==1&&grid[i-1][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
-                    else if (ans[i][j-1]==1&&grid[i][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
-                    else if (i<grid.size()-1&&ans[i+1][j-1]==1&&grid[i+1][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
+                    if (ans[i][j]==0)
+                    {
+                        if (i>0&&ans[i-1][j-1]==1&&grid[i-1][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
+                        else if (ans[i][j-1]==1&&grid[i][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
+                        else if (i<grid.size()-1&&ans[i+1][j-1]==1&&grid[i+1][j-1]<grid[i][j]) {ans[i][j]=1; temp=j;}
+                    }
                 }
                 if (temp<j) break;
             }
             m=max(m, temp);
             if (m==grid[0].size()-1) return m;
-            ans=zero;
+            //ans=zero;
         }
         return m;
     }
