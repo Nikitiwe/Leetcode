9c08c1c2aa5d83ec19041b695d52ccc2077a46dd
Nikitiwe <rvnggm@gmail.com>
2024-10-29 20:01:30 +0700

    Time: 10 ms (50%), Space: 17.9 MB (17.95%) - LeetHub

---
+++ 1527-number-of-ways-to-paint-n-3-grid/1527-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        vector<vector<long>> ans(n, vector<long> (12, 0));
+        for (int j=0; j!=12; j++) ans[0][j]=1;
+        for (int i=1; i<n; i++)
+        {
+            ans[i][0]+=ans[i-1][4]+ans[i-1][5]+ans[i-1][7]+ans[i-1][8]+ans[i-1][9];
+            ans[i][1]+=ans[i-1][4]+ans[i-1][6]+ans[i-1][7]+ans[i-1][8];
+            ans[i][2]+=ans[i-1][4]+ans[i-1][5]+ans[i-1][8]+ans[i-1][9]+ans[i-1][11];
+            ans[i][3]+=ans[i-1][5]+ans[i-1][9]+ans[i-1][10]+ans[i-1][11];
+            ans[i][4]+=ans[i-1][0]+ans[i-1][1]+ans[i-1][2]+ans[i-1][10]+ans[i-1][11];
+            ans[i][5]+=ans[i-1][0]+ans[i-1][2]+ans[i-1][3]+ans[i-1][10];
+            ans[i][6]+=ans[i-1][1]+ans[i-1][8]+ans[i-1][9]+ans[i-1][11];
+            ans[i][7]+=ans[i-1][0]+ans[i-1][1]+ans[i-1][9]+ans[i-1][10]+ans[i-1][11];
+            ans[i][8]+=ans[i-1][0]+ans[i-1][1]+ans[i-1][2]+ans[i-1][6];
+            ans[i][9]+=ans[i-1][0]+ans[i-1][2]+ans[i-1][3]+ans[i-1][6]+ans[i-1][7];
+            ans[i][10]+=ans[i-1][3]+ans[i-1][4]+ans[i-1][5]+ans[i-1][7];
+            ans[i][11]+=ans[i-1][2]+ans[i-1][3]+ans[i-1][4]+ans[i-1][6]+ans[i-1][7];
+
+            for (int j=0; j!=12; j++)
+            {
+                ans[i][j]%=1000000007;
+            }
+        }
+        long t=0;
+        for (int j=0; j!=12; j++) t+=ans[n-1][j];
+        return t%1000000007;
+    }
+};
