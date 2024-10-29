ed0db43b17d1ec1a10f7405b88a072328875d6dd
Nikitiwe <rvnggm@gmail.com>
2024-10-29 16:30:41 +0700

    Time: 1 ms (87.5%), Space: 17.1 MB (97.46%) - LeetHub

--- 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& g) {
-        vector<vector<int>> ans(g.size(), vector<int> (g[0].size(), 10000000));
-        for (int j=0; j!=g[0].size(); j++) ans[0][j]=g[0][j];
+        vector<int> ans(g[0].size(), 10000000), prev=ans;
+        for (int j=0; j!=g[0].size(); j++) prev[j]=g[0][j];
         for (int i=1; i<g.size(); i++)
         {
+            int id=0;
             for (int j=0; j!=g[0].size(); j++)
             {
-                int t=1000000;
-                for (int k=0; k!=g[0].size(); k++) if (j!=k) t=min(t, ans[i-1][k]);
-                ans[i][j]=t;
-                ans[i][j]+=g[i][j];
+                if (prev[j]<prev[id]) id=j;
             }
+            for (int j=0; j!=g[0].size(); j++)
+            {
+                if (j!=id)
+                {
+                    ans[j]=prev[id];
+                }
+                else
+                {
+                    int t=10000000;
+                    for (int k=0; k!=g[0].size(); k++) if (j!=k) t=min(t, prev[k]);
+                    ans[j]=t;
+                }
+                ans[j]+=g[i][j];
+            }
+            prev=ans;
         }
-        int t=1000000;
-        for (int j=0; j!=g[0].size(); j++) t=min(t, ans[g.size()-1][j]);
+        int t=10000000;
+        if (g.size()>1) for (int j=0; j!=g[0].size(); j++) t=min(t, ans[j]);
+        else for (int j=0; j!=g[0].size(); j++) t=min(t, prev[j]);
         return t;
     }
 };
