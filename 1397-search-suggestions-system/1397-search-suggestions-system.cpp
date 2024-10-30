9541f4ad600bd0fa236503e46e35e41d14214291
Nikitiwe <rvnggm@gmail.com>
2024-10-30 14:21:38 +0700

    Time: 72 ms (56.28%), Space: 30.3 MB (80.56%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.cpp
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -4,14 +4,20 @@ public:
         sort(s.begin(), s.end());
         vector<vector<string>> ans;
         string t;
+        int r=0, x=0;
         for (int i=0; i!=w.size(); i++)
         {
             t+=w[i];
             vector<string> arr;
-            for (int j=0; j!=s.size(); j++)
+            for (int j=r; j<s.size(); j++)
             {
                 int l=0;
                 while (l<s[j].size()&&l<t.size()&&s[j][l]==t[l]) l++;
+                if (l>x)
+                {
+                    x=l;
+                    r=j;
+                }
                 if (l==t.size()) arr.push_back(s[j]);
                 if (arr.size()==3)
                 {
