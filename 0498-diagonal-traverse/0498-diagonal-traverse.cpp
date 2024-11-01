0589a409d0ec573827c842af326ccbe51f52b0ac
Nikitiwe <rvnggm@gmail.com>
2024-11-01 15:50:00 +0700

    Time: 352 ms (5.18%), Space: 22.2 MB (58.3%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& m) {
+        vector<int> ans;
+        for (int k=0; k!=m.size()+m[0].size(); k++)
+        {
+            if (k%2==0)
+            {
+                for (int i=0; i<=k; i++)
+                {
+                    if (k-i<m.size()&&i<m[0].size()) ans.push_back(m[k-i][i]);
+                }
+            }
+            else
+            {
+                for (int i=0; i<=k; i++)
+                {
+                    if (i<m.size()&&k-i<m[0].size()) ans.push_back(m[i][k-i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
