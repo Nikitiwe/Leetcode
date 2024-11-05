49744233f41359c7a057a90c34388fbccec3ac59
Nikitiwe <rvnggm@gmail.com>
2024-11-05 21:16:29 +0700

    Time: 0 ms (100%), Space: 66 MB (69.31%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -5,17 +5,17 @@ public:
         int l=0, c=0, t=arr[0];
         while (1)
         {
-            if (arr[(l+1)%arr.size()]<t)
+            l++;
+            if (arr[l%arr.size()]<t)
             {
                 c++;
             }
             else
             {
                 c=1;
-                t=arr[(l+1)%arr.size()];
+                t=arr[l%arr.size()];
             }
             if (c==k) return t;
-            l++;
         }
         return -1;
     }
