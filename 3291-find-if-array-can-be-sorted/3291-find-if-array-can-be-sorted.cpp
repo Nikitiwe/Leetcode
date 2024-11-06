203097d09a68a6970722828c300a7a1068296936
Nikitiwe <rvnggm@gmail.com>
2024-11-06 22:27:10 +0700

    Time: 4 ms (34.53%), Space: 31.9 MB (28.99%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        vector<vector<int>> arr(nums.size(), vector<int> (2, 0));
-        for (int i=0; i!=nums.size(); i++)
+        vector<int> k, m;
+        int l=0, ma=nums[0], mi=ma, prev=0;
+        int t=nums[0];
+        int s=0;
+        while (t>0)
         {
-            arr[i][0]=nums[i];
-            int t=nums[i];
-            int s=0;
+            prev+=t%2;
+            t/=2;
+        }
+        for (int i=1; i<nums.size(); i++)
+        {
+            t=nums[i];
+            s=0;
             while (t>0)
             {
                 s+=t%2;
                 t/=2;
             }
-            arr[i][1]=s;
-        }
-        vector<int> k, m;
-        int l=0, ma=arr[l][0], mi=ma;
-        for (int i=1; i<nums.size(); i++)
-        {
-            if (arr[i][1]==arr[l][1])
+            if (prev==s)
             {
-                ma=max(arr[i][0], ma);
-                mi=min(arr[i][0], mi);
+                ma=max(nums[i], ma);
+                mi=min(nums[i], mi);
             }
             else
             {
                 k.push_back(ma);
                 m.push_back(mi);
                 l=i;
-                ma=arr[l][0];
-                mi=arr[l][0];
+                prev=s;
+                ma=nums[i];
+                mi=nums[i];
             }
         }
         k.push_back(ma);
@@ -37,4 +39,4 @@ public:
         for (int i=0; i<k.size()-1; i++) if (k[i]>m[i+1]) return 0;
         return 1;
     }
-}; // чот не так пошло ну и пох
+};
