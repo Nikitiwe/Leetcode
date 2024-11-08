5d38f3c9af5008a1be5333e704d2f8fa8fc4e849
Nikitiwe <rvnggm@gmail.com>
2024-11-08 14:13:51 +0700

    Time: 1549 ms (6.47%), Space: 21.2 MB (99.7%) - LeetHub

--- 0976-minimum-area-rectangle/0976-minimum-area-rectangle.cpp
+++ 0976-minimum-area-rectangle/0976-minimum-area-rectangle.cpp
@@ -6,15 +6,18 @@ public:
         {
             for (int j=i+1; j<p.size(); j++)
             {
-                int tt=abs(p[i][1]-p[j][1]);
-                if (p[i][0]==p[j][0]) for (int k=i+1; k<p.size(); k++)
+                if (p[i][0]==p[j][0])
                 {
-                    int t=tt*abs(p[i][0]-p[k][0]);
-                    if (t>0&&t<ans) if (p[i][1]==p[k][1]) for (int a=0; a<p.size(); a++)
+                    int tt=abs(p[i][1]-p[j][1]);
+                    for (int k=i+1; k<p.size(); k++)
                     {
-                        if (p[a][0]==p[k][0]&&p[a][1]==p[j][1])
+                        int t=tt*abs(p[i][0]-p[k][0]);
+                        if (t>0&&t<ans) if (p[i][1]==p[k][1]) for (int a=0; a<p.size(); a++)
                         {
-                            ans=min(ans, t);
+                            if (p[a][0]==p[k][0]&&p[a][1]==p[j][1])
+                            {
+                                ans=min(ans, t);
+                            }
                         }
                     }
                 }
