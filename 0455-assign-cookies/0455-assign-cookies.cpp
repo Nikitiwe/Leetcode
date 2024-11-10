ad340977d526418390892d5d105ad2efa4054c1e
Nikitiwe <rvnggm@gmail.com>
2024-11-10 15:06:32 +0700

    Time: 8 ms (64.12%), Space: 44.7 MB (74.36%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int l=0, r=0, ans=0;
+        while (l<s.size()&&r<g.size())
+        {
+            if (s[l]>=g[r])
+            {
+                ans++;
+                l++;
+                r++;
+            }
+            else l++;
+        }
+        return ans;
+    }
+};
