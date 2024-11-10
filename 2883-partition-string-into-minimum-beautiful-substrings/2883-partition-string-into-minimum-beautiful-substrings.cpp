af3dc3275b30d6c7f2bc745449cf716217b77948
Nikitiwe <rvnggm@gmail.com>
2024-11-10 14:42:34 +0700

    Time: 4 ms (34.88%), Space: 9.6 MB (63.23%) - LeetHub

--- 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.cpp
@@ -9,29 +9,26 @@ public:
             ans[0]=0;
         }
         vector<string> arr{"1", "101", "11001", "1111101", "1001110001", "110000110101", "11110100001001"};
-        for (int i=2; i<ans.size(); i++)
+        for (int i=1; i<ans.size(); i++)
         {
-            for (int k=1; k<=i; k++)
-            {
                 string t;
-                for (int j=k; j<=i; j++)
+                for (int j=i; j<ans.size(); j++)
                 {
                     t+=s[j-1];
-                }
-                bool isit=0;
-                for (int j=0; j!=arr.size(); j++)
-                {
-                    if (t==arr[j])
+                    bool isit=0;
+                    for (int k=0; k!=arr.size(); k++)
                     {
-                        isit=1;
-                        break;
+                        if (t==arr[k])
+                        {
+                            isit=1;
+                            break;
+                        }
+                    }
+                    if (isit==1)
+                    {
+                        ans[j]=min(ans[j], ans[i-1]+1);
                     }
                 }
-                if (isit==1)
-                {
-                    ans[i]=min(ans[i], ans[k-1]+1);
-                }
-            }
         }
         if (ans[ans.size()-1]!=17) return ans[ans.size()-1];
         else return -1;
