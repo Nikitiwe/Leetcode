2ac477197e3a0e585ee61d7d642d4a67e6083996
Nikitiwe <rvnggm@gmail.com>
2024-11-13 04:39:25 +0700

    Time: 47 ms (7.33%), Space: 18.2 MB (88.76%) - LeetHub

---
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        string ans;
+        for (int i=0; i!=d.size(); i++)
+        {
+            int l=0, r=0;
+            while (l<s.size()&&r<d[i].size())
+            {
+                if (s[l]==d[i][r])
+                {
+                    l++;
+                    r++;
+                }
+                else
+                {
+                    l++;
+                }
+                if (r==d[i].size())
+                {
+                    if (ans.size()<d[i].size()) ans=d[i];
+                    else if (ans.size()==d[i].size()) ans=min(ans, d[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
