8ba4922db3b20b5633011cf2cb544b7b73f85a4c
Nikitiwe <rvnggm@gmail.com>
2024-11-13 04:40:41 +0700

    Time: 25 ms (19.83%), Space: 18.3 MB (74.66%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.cpp
@@ -4,6 +4,8 @@ public:
         string ans;
         for (int i=0; i!=d.size(); i++)
         {
+            if (ans.size()<=d[i].size())
+            {
             int l=0, r=0;
             while (l<s.size()&&r<d[i].size())
             {
@@ -21,6 +23,7 @@ public:
                     if (ans.size()<d[i].size()) ans=d[i];
                     else if (ans.size()==d[i].size()) ans=min(ans, d[i]);
                 }
+            }
             }
         }
         return ans;
