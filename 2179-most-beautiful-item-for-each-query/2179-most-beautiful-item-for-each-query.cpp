b98170bf0d039533e96fd51ed0b34b706f407ae5
Nikitiwe <rvnggm@gmail.com>
2024-11-14 17:21:06 +0700

    Time: 40 ms (95.53%), Space: 91.3 MB (96.68%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -9,48 +9,29 @@ public:
             if (nums[i][0]==arr[arr.size()-1][0]) arr[arr.size()-1][1]=max(arr[arr.size()-1][1], nums[i][1]);
             else if (nums[i][1]>arr[arr.size()-1][1]) arr.push_back(nums[i]);
         }
-        /*vector<int> f(arr.size(), 0), s(arr.size(), 0);
-        for (int i=0; i!=arr.size(); i++)
-        {
-            f[i]=arr[i][0];
-            s[i]=arr[i][1];
-        }
-        return s;*/
         for (int i=0; i!=q.size(); i++)
         {
             if (q[i]<arr[0][0]) q[i]=0;
             else if (q[i]>=arr[arr.size()-1][0]) q[i]=arr[arr.size()-1][1];
             else
             {
-                int l=0, r=arr.size()-1, m=(l+r)/2;
-                while (l<r-1)
+                int l=0, r=arr.size()-1, m=(l+r)/2, ans=0;
+                while (l<r)
                 {
                     m=(l+r)/2;
-                    if (arr[m][0]>q[i]) r=m-1;
-                    else if (arr[m][0]==q[i]) break;
-                    else if (arr[m][0]<q[i]) l=m;
-                }
-                if (arr[m][0]==q[i]) q[i]=arr[m][1];
-                else if (arr[m][0]<q[i])
-                {
-                    while (m+1<arr.size()&&arr[m+1][0]<=q[i]) m++;
-                    q[i]=arr[m][1];
-                    /*if (m+1<arr.size()&&arr[m+1][0]<=q[i]) q[i]=arr[m+1][1];
-                    else q[i]=arr[m][1];*/
-                }
-                else if (arr[m][0]>q[i])
-                {
-                    if (m==0) q[i]=0;
-                    else
+                    if (arr[m][0]>q[i])
                     {
-                        if (arr[m-1][0]<=q[i]) q[i]=arr[m-1][1];
-                        else
-                        {
-                            if (m==1) q[i]=0;
-                            else q[i]=arr[m-2][1];
-                        }
+                        r=m-1;
+                    }
+                    else if (arr[m][0]<=q[i])
+                    {
+                        ans=max(ans, arr[m][1]);
+                        l=m+1;
                     }
                 }
+                m=(l+r)/2;
+                if (arr[m][0]<=q[i]) ans=max(ans, arr[m][1]);
+                q[i]=ans;
             }
         }
         return q;
