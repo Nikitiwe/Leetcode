5e15a1a0c8bbc853773f00647e82adb4d9f4b365
Nikitiwe <rvnggm@gmail.com>
2024-11-16 17:20:24 +0700

    Time: 0 ms (100%), Space: 7.6 MB (25.6%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n<=0) return 0;
-        while (n>1)
-        {
-            if (n%4!=0) return 0;
-            n/=4;
-        }
-        return 1;
+        if(n==1||n==4||n==16||n==64||n==256||n==1024||n==4096||n==16384||n==65536||n==262144||n==1048576
+        ||n==4194304||n==16777216||n==67108864||n==268435456||n==1073741824||n==4294967296) return 1;
+        else return 0;
     }
 };
