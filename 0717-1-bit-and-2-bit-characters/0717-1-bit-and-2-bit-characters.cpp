63ec7c615fda11300bd164e5685635e45998288f
Nikitiwe <rvnggm@gmail.com>
2024-11-19 16:42:42 +0700

    Time: 0 ms (100%), Space: 12.2 MB (87.85%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -3,12 +3,12 @@ public:
     bool isOneBitCharacter(vector<int>& s) {
         if (s.size()==1) return 1-s[0];
         int l=0;
-        while (l<s.size())
+        while (l<s.size()-1)
         {
             if (s[l]==0) l++;
             else l+=2;
-            if (l==s.size()-1&&s.back()==0) return 1;
         }
+        if (l==s.size()-1&&s.back()==0) return 1;
         return 0;
     }
 };
