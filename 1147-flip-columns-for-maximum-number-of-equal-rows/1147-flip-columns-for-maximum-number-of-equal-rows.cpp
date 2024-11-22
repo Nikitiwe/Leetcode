a5f96fb64927cdb227f9eeb076e9a26eee85906a
Nikitiwe <rvnggm@gmail.com>
2024-11-23 04:40:10 +0700

    Time: 12 ms (88.73%), Space: 75.6 MB (57.3%) - LeetHub

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& m) {
+        unordered_map<string, int> n;
+        int ans=0;
+        for (int i=0; i!=m.size(); i++)
+        {
+            string t;
+            for (int j=0; j!=m[i].size(); j++)
+            {
+                t+=m[i][j]+' ';
+                m[i][j]=1-m[i][j];
+            }
+            n[t]++;
+            ans=max(ans, n[t]);
+            t="";
+            for (int j=0; j!=m[i].size(); j++)
+            {
+                t+=m[i][j]+' ';
+            }
+            n[t]++;
+            ans=max(ans, n[t]);
+        }
+        return ans;
+    }
+};
