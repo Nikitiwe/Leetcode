1a06f761b3ba2407cee8c37de3b30b9a55cf551f
Nikitiwe <rvnggm@gmail.com>
2024-12-16 17:46:46 +0700

    Time: 643 ms (20.69%), Space: 199.9 MB (12.05%) - LeetHub

---
+++ 1830-count-good-meals/1830-count-good-meals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums) {
+        long long ans=0, k=1;
+        unordered_map<int, int> m;
+        for (int i=0; i!=nums.size(); i++) m[nums[i]]++;
+        for (int j=1; j<=22; j++)
+        {
+            for (int i=0; i!=nums.size(); i++)
+            {
+                if (k-nums[i]!=nums[i])
+                {
+                    ans+=m[k-nums[i]];
+                }
+                else
+                {
+                    ans+=(long long)(m[k-nums[i]]-1);
+                }
+            }
+            k*=2;
+        }
+        return (ans/2)%1000000007;
+    }
+};
