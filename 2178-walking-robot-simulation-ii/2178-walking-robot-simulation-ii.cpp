1110001a6d73d0d3ec053f70faba863f37dec049
Nikitiwe <rvnggm@gmail.com>
2024-12-24 17:58:20 +0700

    Time: 27 ms (77.53%), Space: 128.4 MB (24.72%) - LeetHub

---
+++ 2178-walking-robot-simulation-ii/2178-walking-robot-simulation-ii.cpp
@@ -0,0 +1,72 @@
+class Robot {
+private:
+    vector<vector<int>> g;
+    int d, x, y, w, h;
+public:
+    Robot(int ww, int hh) {
+        d=0, y=0, x=0, w=ww, h=hh;
+        vector<int> row(w, 0);
+        for (int i=0; i!=h; i++)
+        {
+            g.push_back(row);
+        }
+    }
+    
+    void step(int n) {
+        while (n>0)
+        {
+            if (x==0&&y==0&&d==0&&n>2*(w+h)-4)
+            {
+                n%=2*(w+h)-4;
+                d=3;
+            }
+            else if (d==0)
+            {
+                int t=min(n, w-1-x);
+                x+=t;
+                n-=t;
+                if (n>0) d++;
+            }
+            else if (d==1)
+            {
+                int t=min(n, h-1-y);
+                y+=t;
+                n-=t;
+                if (n>0) d++;
+            }
+            else if (d==2)
+            {
+                int t=min(n, x);
+                x-=t;
+                n-=t;
+                if (n>0) d++;
+            }
+            else
+            {
+                int t=min(n, y);
+                y-=t;
+                n-=t;
+                if (n>0) d=0;
+            }
+        }
+    }
+    
+    vector<int> getPos() {
+        return {x, y};
+    }
+    
+    string getDir() {
+        if (d==0) return "East";
+        else if (d==1) return "North";
+        else if (d==2) return "West";
+        else return "South";
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
