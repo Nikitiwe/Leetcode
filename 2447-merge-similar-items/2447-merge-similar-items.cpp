b32e6246e5090a3233ee1d38bf67b9421bf0dd70
Nikitiwe <rvnggm@gmail.com>
2025-01-08 02:09:26 +0700

    Time: 12 ms (19.25%), Space: 20.9 MB (60.57%) - LeetHub

---
+++ 2447-merge-similar-items/2447-merge-similar-items.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& i1, vector<vector<int>>& i2) {
+        unordered_map<int, int> m;
+        for (int i=0; i!=i1.size(); i++) m[i1[i][0]]+=i1[i][1];
+        for (int i=0; i!=i2.size(); i++) m[i2[i][0]]+=i2[i][1];
+        vector<vector<int>> ans;
+        for (auto i=m.begin(); i!=m.end(); i++) ans.push_back({i->first, i->second});
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
