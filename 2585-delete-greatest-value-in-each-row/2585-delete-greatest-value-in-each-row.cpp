10e6c30b3b759f612f3403226a49919a8ca057e8
Nikitiwe <rvnggm@gmail.com>
2025-01-07 16:24:35 +0700

    Time: 5 ms (28.17%), Space: 14.5 MB (16.02%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& g) {
         int ans=0;
+        vector<priority_queue<int>> m(g.size(), priority_queue<int>());
+        for (int i=0; i!=g.size(); i++)
+        {
+            for (int j=0; j!=g[0].size(); j++)
+            {
+                m[i].push(g[i][j]);
+            }
+        }
         for (int k=0; k<g[0].size(); k++)
         {
             int t=0;
             for (int i=0; i!=g.size(); i++)
             {
-                int id=0;
-                for (int j=0; j!=g[0].size(); j++)
-                {
-                    if (g[i][j]>g[i][id]) id=j;
-                }
-                t=max(t, g[i][id]);
-                g[i][id]=0;
+                t=max(t, m[i].top());
+                m[i].pop();
             }
             ans+=t;
         }
