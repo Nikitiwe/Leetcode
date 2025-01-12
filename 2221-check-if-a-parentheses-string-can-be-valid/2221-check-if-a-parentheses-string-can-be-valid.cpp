fa5a793f590fb3261e000d5af19722f323fb9e8c
Nikitiwe <rvnggm@gmail.com>
2025-01-13 04:56:35 +0700

    Time: 10 ms (81.16%), Space: 30.1 MB (70.6%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    bool canBeValid(string s, string arr) {
+    bool canBeValid(string s, string loc) {
         if (s.size()%2==1) return 0;
-        int a=0;
+        int l=0, r=0;
         for (int i=0; i!=s.size(); i++)
         {
-            if (s[i]==')'&&arr[i]=='1') a--;
-            else a++;
-            if (a<0) return 0;
+            if (loc[i]=='1')
+            {
+                if (s[i]=='(') l++;
+                else r++;
+            }
+            else l++;
+            if (r>l) return 0;
         }
-        a=0;
+        l=0; r=0;
         for (int i=s.size()-1; i>=0; i--)
         {
-            if (s[i]=='('&&arr[i]=='1') a--;
-            else a++;
-            if (a<0) return 0;
+            if (loc[i]=='1')
+            {
+                if (s[i]==')') r++;
+                else l++;
+            }
+            else r++;
+            if (l>r) return 0;
         }
         return 1;
     }
