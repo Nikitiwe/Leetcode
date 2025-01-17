dada6f511a59d1b52f3812c1b1d83e4cc48403d9
Nikitiwe <rvnggm@gmail.com>
2025-01-18 04:00:04 +0700

    Time: 4 ms (50%), Space: 264.1 MB (25.63%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& nums) {
+        int a=0;
+        for (int i=0; i!=nums.size(); i++) a^=nums[i];
+        if (a==0) return 1;
+        else return 0;
+    }
+};
