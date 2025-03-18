16a1b200fd1b09c8b9eed0c676652ec5a3221e50
Nikitiwe <rvnggm@gmail.com>
2025-03-18 19:34:57 +0700

    Time: 11 ms (53.42%), Space: 43.2 MB (89.63%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& g) {
+        vector<vector<int>> d = g;
+        for (int i=0; i!=g.size(); i++)
+        {
+            priority_queue<int> q;
+            for (int j=0; i+j<g.size(); j++)
+            {
+                q.push(g[i+j][j]);
+            }
+            for (int j=0; i+j<g.size(); j++)
+            {
+                d[i+j][j] = q.top();
+                q.pop();
+            }
+        }
+        for (int i=1; i<g.size(); i++)
+        {
+            priority_queue<int> q;
+            for (int j=0; i+j<g.size(); j++)
+            {
+                q.push(1000000 - g[j][i+j]);
+            }
+            for (int j=0; i+j<g.size(); j++)
+            {
+                d[j][i+j] = 1000000 - q.top();
+                q.pop();
+            }
+        }
+        return d;
+    }
+};
