bd7a936afa47933977ef38fb38f7978665284c14
Nikitiwe <rvnggm@gmail.com>
2025-04-02 14:27:14 +0700

    Time: 15 ms (92.89%), Space: 33.8 MB (75.69%) - LeetHub

--- 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.cpp
@@ -12,23 +12,23 @@
 class Solution {
 public:
     int res = 0;
-    vector<vector<int>> f(TreeNode* root, int d)
+    vector<int> f(TreeNode* root, int d)
     {
         if (root == nullptr) return {};
-        vector<vector<int>> ans, l = f(root->left, d), r = f(root->right, d);
+        vector<int> ans, l = f(root->left, d), r = f(root->right, d);
         for (int i=0; i<l.size(); i++)
         {
             for (int j=0; j<r.size(); j++)
             {
-                if (l[i][1] + r[j][1] + 2 <= d) res++;
+                if (l[i] + r[j] + 2 <= d) res++;
             }
-            if (l[i][1]+1 < d) ans.push_back({l[i][0], l[i][1]+1});
+            if (l[i]+1 < d) ans.push_back(l[i]+1);
         }
         for (int j=0; j<r.size(); j++)
         {
-            if (r[j][1]+1 < d) ans.push_back({r[j][0], r[j][1]+1});
+            if (r[j]+1 < d) ans.push_back(r[j]+1);
         }
-        if (root->left == nullptr && root->right == nullptr) ans.push_back({root->val, 0});
+        if (root->left == nullptr && root->right == nullptr) ans.push_back(0);
         return ans;
     }
 
