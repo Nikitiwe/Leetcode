72d3efdf8c75c350be9634b59f5b4539df60150c
Nikitiwe <rvnggm@gmail.com>
2025-04-06 20:28:00 +0700

    Time: 538 ms (31.7%), Space: 112.2 MB (37.02%) - LeetHub

--- 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.cpp
@@ -13,21 +13,21 @@
 class Solution {
 public:
     int ans = 0;
-    static const vector<int> no;
-    static const vector<int> zero;
+    static const array<int, 4> no;
+    static const array<int, 4> zero;
     
-    vector<int> f(TreeNode* root) // mi, ma, sum
+    array<int, 4> f(TreeNode* root) // mi, ma, sum, isit
     {
         if (root == nullptr) return zero;
-        vector<int> l = f(root->left);
-        vector<int> r = f(root->right);
+        array<int, 4> l = f(root->left);
+        array<int, 4> r = f(root->right);
         if (l == no || r == no) return no;
         if (l == zero && r != zero)
         {
             if (root->val < r[0])
             {
                 ans = max(ans, root->val + r[2]);
-                return {root->val, r[1], root->val + r[2]};
+                return {root->val, r[1], root->val + r[2], 1};
             }
             else return no;
         }
@@ -36,7 +36,7 @@ public:
             if (root->val > l[1])
             {
                 ans = max(ans, root->val + l[2]);
-                return {l[0], root->val, root->val + l[2]};
+                return {l[0], root->val, root->val + l[2], 1};
             }
             else return no;
         }
@@ -45,14 +45,14 @@ public:
             if (root->val > l[1] && root->val < r[0])
             {
                 ans = max(ans, root->val + l[2] + r[2]);
-                return {l[0], r[1], root->val + l[2] + r[2]};
+                return {l[0], r[1], root->val + l[2] + r[2], 1};
             }
             else return no;
         }
         else
         {
             ans = max(ans, root->val);
-            return {root->val, root->val, root->val};
+            return {root->val, root->val, root->val, 1};
         }
     }
 
@@ -61,5 +61,5 @@ public:
         return ans;
     }
 };
-const vector<int> Solution::no = { -1 };
-const vector<int> Solution::zero = { 0 };
+const std::array<int, 4> Solution::no = {0, 0, 0, -1};
+const std::array<int, 4> Solution::zero = {0, 0, 0, 0};
