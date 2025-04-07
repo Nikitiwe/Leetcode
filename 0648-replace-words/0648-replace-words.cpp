e785a54e54d2baed756eb7c3493bf508c949a07a
Nikitiwe <rvnggm@gmail.com>
2025-04-07 16:35:08 +0700

    Time: 59 ms (55.4%), Space: 68.9 MB (83.53%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,29 +1,53 @@
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool isroot = 0;
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& d, string s) {
-        unordered_set<string> m(d.begin(), d.end());
-        s+=' ';
-        string ans, t, temp="";
+        TrieNode* root = new TrieNode();
+        for (int j=0; j!=d.size(); j++)
+        {
+            TrieNode* t = root;
+            for (int i=0; i!=d[j].size(); i++)
+            {
+                if ( (t->children).count(d[j][i]) == 0)
+                {
+                    TrieNode* n = new TrieNode();
+                    (t->children)[d[j][i]] = n;
+                    t = n;
+                }
+                else t = (t->children)[d[j][i]];
+            }
+            t->isroot = 1;
+        }
+        s += " ";
+        string ans = "", t = "";
         for (int i=0; i!=s.size(); i++)
         {
-            if (temp.size()==0&&s[i]!=' ')
+            if (s[i] != ' ') t +=s[i];
+            else
             {
-                t+=s[i];
-                if (m.count(t)>0)
+                if (ans.size() > 0 && ans.back() != ' ') ans += ' ';
+                TrieNode* tt = root;
+                for (int j=0; j!=t.size(); j++)
                 {
-                    temp=t;
+                    if ( (tt->children).count(t[j]) == 1)
+                    {
+                        tt = (tt->children)[t[j]];
+                        if (tt->isroot == 1)
+                        {
+                            t = t.substr(0, j+1);
+                            break;
+                        }
+                    }
+                    else break;
                 }
-            }
-            else if (s[i]==' ')
-            {
-                if (temp.size()==0) ans+=t;
-                else ans+=temp;
-                ans+=' ';
-                t="";
-                temp="";
+                ans += t;
+                t = "";
             }
         }
-        ans.pop_back();
         return ans;
     }
 };
