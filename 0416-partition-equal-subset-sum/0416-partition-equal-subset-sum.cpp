d8f2fbd1a998bdecdecf63d996d3420c54e2e52a
Nikitiwe <rvnggm@gmail.com>
2025-04-07 12:29:11 +0700

    Time: 91 ms (74.15%), Space: 29.8 MB (52.86%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,20 +4,19 @@ public:
         int sum=0;
         for (int i=0; i<nums.size(); i++) sum+=nums[i];
         if (sum%2==1) return 0;
+        sum /= 2;
         unordered_set<int> prev, next;
-        prev.insert(nums[0]);
-        for (int i=1; i<nums.size(); i++)
+        prev.insert(0);
+        for (int i=0; i<nums.size(); i++)
         {
             for (auto it=prev.begin(); it!=prev.end(); it++) 
             {
                 next.insert(*it+nums[i]);
-                next.insert(*it-nums[i]);
             }
-            //if (i<nums.size()-1&&next.count(sum/2)>0) return 1;
+            if (next.count(sum)>0) return 1;
             prev=next;
-            next.clear();
         }
-        if (prev.count(0)>0) return 1;
+        // if (prev.count(0)>0) return 1;
         return 0;
     }
 };
