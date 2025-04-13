130eae317366858dbf7d7d3085be0080cc8ad4f1
Nikitiwe <rvnggm@gmail.com>
2025-04-13 15:41:15 +0700

    Time: 9 ms (35.08%), Space: 11.3 MB (44.5%) - LeetHub

---
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& gg) {
+        int ans = 10;
+        vector<vector<int>> zero(gg.size(), vector<int>(gg[0].size(), 0));
+        for (int k=0; k!=512; k++)
+        {
+            int t = k;
+            int count = 0;
+            vector<vector<int>> g = gg;
+            for (int i=0; i!=g.size(); i++)
+            {
+                for (int j=0; j!=g[i].size(); j++)
+                {
+                    if (t%2 == 1)
+                    {
+                        count++;
+                        g[i][j] = 1 - g[i][j];
+                        if (i>0) g[i-1][j] = 1 - g[i-1][j];
+                        if (j>0) g[i][j-1] = 1 - g[i][j-1];
+                        if (i+1<g.size()) g[i+1][j] = 1 - g[i+1][j];
+                        if (j+1<g[i].size()) g[i][j+1] = 1 - g[i][j+1];
+                    }
+                    t/=2;
+                }
+            }
+            if (g == zero) ans = min(ans, count);
+        }
+        if (ans == 10) return -1;
+        else return ans;
+    }
+};
