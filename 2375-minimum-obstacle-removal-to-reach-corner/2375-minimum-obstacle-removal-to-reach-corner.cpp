90c6aa5c264a01e21ae5e9f9348f97379853c5dc
Nikitiwe <rvnggm@gmail.com>
2025-04-14 17:23:02 +0700

    Time: 466 ms (31.7%), Space: 232.6 MB (24.67%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -6,65 +6,65 @@ public:
     }
     int minimumObstacles(vector<vector<int>>& g) {
         int n = g.size(), m = g[0].size();
-        queue<int> x, y;
+        deque<int> x, y;
         unordered_map<long long, int> ans;
-        x.push(0);
-        y.push(0);
+        x.push_front(0);
+        y.push_front(0);
         ans[0] = 0;
         while (x.size() > 0)
         {
             int a = x.front(), b = y.front();
-            x.pop(); y.pop();
+            x.pop_front(); y.pop_front();
             int t = ans[f(a, b)];
             if (a > 0 && g[a-1][b] == 0 && (ans.count(f(a-1, b)) == 0 || ans[f(a-1, b)] > t))
             {
-                x.push(a-1);
-                y.push(b);
+                x.push_front(a-1);
+                y.push_front(b);
                 ans[f(a-1, b)] = t;
             }
             else if (a > 0 && g[a-1][b] == 1 && (ans.count(f(a-1, b)) == 0 || ans[f(a-1, b)] > t + 1))
             {
-                x.push(a-1);
-                y.push(b);
+                x.push_back(a-1);
+                y.push_back(b);
                 ans[f(a-1, b)] = t + 1;
             }
 
             if (b > 0 && g[a][b-1] == 0 && (ans.count(f(a, b-1)) == 0 || ans[f(a, b-1)] > t))
             {
-                x.push(a);
-                y.push(b-1);
+                x.push_front(a);
+                y.push_front(b-1);
                 ans[f(a, b-1)] = t;
             }
             else if (b > 0 && g[a][b-1] == 1 && (ans.count(f(a, b-1)) == 0 || ans[f(a, b-1)] > t + 1))
             {
-                x.push(a);
-                y.push(b-1);
+                x.push_back(a);
+                y.push_back(b-1);
                 ans[f(a, b-1)] = t + 1;
             }
 
             if (a+1 < n && g[a+1][b] == 0 && (ans.count(f(a+1, b)) == 0 || ans[f(a+1, b)] > t))
             {
-                x.push(a+1);
-                y.push(b);
+                x.push_front(a+1);
+                y.push_front(b);
                 ans[f(a+1, b)] = t;
             }
             else if (a+1 < n && g[a+1][b] == 1 && (ans.count(f(a+1, b)) == 0 || ans[f(a+1, b)] > t + 1))
             {
-                x.push(a+1);
-                y.push(b);
+                x.push_back(a+1);
+                y.push_back(b);
                 ans[f(a+1, b)] = t + 1;
             }
 
             if (b+1 < m && g[a][b+1] == 0 && (ans.count(f(a, b+1)) == 0 || ans[f(a, b+1)] > t))
             {
-                x.push(a);
-                y.push(b+1);
+                x.push_front(a);
+                y.push_front(b+1);
                 ans[f(a, b+1)] = t;
             }
             else if (b+1 < m && g[a][b+1] == 1 && (ans.count(f(a, b+1)) == 0 || ans[f(a, b+1)] > t + 1))
             {
-                x.push(a);
-                y.push(b+1);
+                x.push_back(a);
+                y.push_back(b+1);
                 ans[f(a, b+1)] = t + 1;
             }            
         }
