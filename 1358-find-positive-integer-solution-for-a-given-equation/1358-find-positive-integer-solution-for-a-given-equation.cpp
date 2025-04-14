7b269f818dcb5ad3eb4a71f4f7ff5f3b0e6aa2c6
Nikitiwe <rvnggm@gmail.com>
2025-04-14 17:51:45 +0700

    Time: 0 ms (100%), Space: 9.1 MB (41.3%) - LeetHub

---
+++ 1358-find-positive-integer-solution-for-a-given-equation/1358-find-positive-integer-solution-for-a-given-equation.cpp
@@ -0,0 +1,36 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& f, int z) {
+        vector<vector<int>> res;
+        for (int i=1; i<=1000; i++)
+        {
+            int l = 1, r = 1000, m;
+            if (f.f(i, l) > z) break;
+            int ans = 5000;
+            while (l <= r)
+            {
+                m = (l+r)/2;
+                if (f.f(i, m) >= z)
+                {
+                    ans = min(ans, m);
+                    r = m-1;
+                }
+                else l = m+1;
+            }
+            if (f.f(i, ans) == z) res.push_back({i, ans});
+        }
+        return res;
+    }
+};
