0f4e3110176e677452a0da800c0807d4173e9145
Nikitiwe <rvnggm@gmail.com>
2025-04-15 23:01:57 +0700

    Time: 26 ms (58.98%), Space: 37.4 MB (63.05%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -7,20 +7,16 @@ public:
         {
             m[r[i][0]]++;
             m[r[i][1]]++;
-            s.insert(r[i][0]*1000 + r[i][1]);
-            s.insert(r[i][1]*1000 + r[i][0]);
+            s.insert(min(r[i][0], r[i][1])*1000 + max(r[i][0], r[i][1]));
         }
         int ans = 0;
         for (int i=0; i!=n; i++)
         {
-            for (int j=0; j!=n; j++)
+            for (int j=i+1; j<n; j++)
             {
-                if (i != j)
-                {
-                    int t = m[i] + m[j];
-                    if (s.count(i*1000 + j) == 1 || s.count(j*1000 + i) == 1) t--;
-                    ans = max(ans, t);
-                }
+                int t = m[i] + m[j];
+                if (s.count(i*1000 + j) == 1) t--;
+                ans = max(ans, t);
             }
         }
         return ans;
