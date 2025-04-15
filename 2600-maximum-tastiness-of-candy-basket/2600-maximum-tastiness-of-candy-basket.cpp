560f87a9ec93d765f3d9bfb4a93f7553757330b4
Nikitiwe <rvnggm@gmail.com>
2025-04-15 21:24:00 +0700

    Time: 43 ms (85.43%), Space: 52.3 MB (26.87%) - LeetHub

---
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumTastiness(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, l = 0, r = 1000000000, m;
+        while (l <= r)
+        {
+            m = (l+r)/2;
+            int t = nums[0], c = 1;
+            for (int i=1; i<nums.size(); i++)
+            {
+                if (nums[i] >= t + m)
+                {
+                    t = nums[i];
+                    c++;
+                }
+            }
+            if (c >= k)
+            {
+                ans = max(ans, m);
+                l = m+1;
+            }
+            else r = m-1;
+        }
+        return ans;
+    }
+};
