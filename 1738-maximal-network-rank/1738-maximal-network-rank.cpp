0c46387c22e5087d26cba9c09b54837a052605e3
Nikitiwe <rvnggm@gmail.com>
2025-04-15 23:00:07 +0700

    Time: 34 ms (47.8%), Space: 42.3 MB (47.68%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.cpp
+++ 1738-maximal-network-rank/1738-maximal-network-rank.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& r) {
         unordered_map<int, int> m;
+        unordered_set<int> s;
         for (int i=0; i!=r.size(); i++)
         {
             m[r[i][0]]++;
             m[r[i][1]]++;
+            s.insert(r[i][0]*1000 + r[i][1]);
+            s.insert(r[i][1]*1000 + r[i][0]);
         }
         int ans = 0;
         for (int i=0; i!=n; i++)
@@ -15,14 +18,7 @@ public:
                 if (i != j)
                 {
                     int t = m[i] + m[j];
-                    for (int a=0; a!=r.size(); a++)
-                    {
-                        if ((r[a][0] == i && r[a][1] == j) || (r[a][0] == j && r[a][1] == i))
-                        {
-                            t--;
-                            break;
-                        }
-                    }
+                    if (s.count(i*1000 + j) == 1 || s.count(j*1000 + i) == 1) t--;
                     ans = max(ans, t);
                 }
             }
