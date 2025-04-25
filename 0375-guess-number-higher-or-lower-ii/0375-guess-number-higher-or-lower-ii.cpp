2c3aa4ef6953c0362ebfee468226c7c643976854
Nikitiwe <rvnggm@gmail.com>
2025-04-25 18:54:59 +0700

    Time: 319 ms (5.19%), Space: 18.6 MB (5.19%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_map<int, int> m;
+    int f(int l, int r)
+    {
+        if (l >= r) return 0;
+        if (l + 1 == r) return l;
+        if (m.count(1000*l + r) == 0)
+        {
+            int ans = 100000000;
+            for (int i=l+1; i<=r-1; i++)
+            {
+                ans = min(ans, i + max(f(l, i-1), f(i+1, r)) );
+            }
+            m[1000*l + r] = ans;
+        }
+        return m[1000*l + r];
+    }
+
+    int getMoneyAmount(int n) {
+        return f(1, n);
+    }
+};
