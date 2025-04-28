7c4ac6c1d11ea2bafe22b59c68ec298d4cfd9c71
Nikitiwe <rvnggm@gmail.com>
2025-04-28 22:12:33 +0700

    Time: 50 ms (97.69%), Space: 252.3 MB (94.23%) - LeetHub

--- 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<int> resultsArray(vector<vector<int>>& q, int k) {
         vector<int> ans(q.size(), -1);
+        if (k > ans.size()) return ans;
         priority_queue<int> arr;
-        for (int i=0; i!=q.size(); i++)
+        for (int i=0; i<k; i++)
         {
             int t = abs(q[i][0]) + abs(q[i][1]);
-            if (i < k) arr.push(t);
-            if (arr.size() == k)
+            arr.push(t);
+        }
+        if (k-1 < ans.size()) ans[k-1] = arr.top();
+        for (int i=k; i<q.size(); i++)
+        {
+            int t = abs(q[i][0]) + abs(q[i][1]);
+            if (arr.top() > t)
             {
-                if (i >= k && arr.top() > t)
-                {
-                    arr.pop();
-                    arr.push(t);
-                }
-                ans[i] = arr.top();
+                arr.pop();
+                arr.push(t);
             }
+            ans[i] = arr.top();
         }
         return ans;
     }
