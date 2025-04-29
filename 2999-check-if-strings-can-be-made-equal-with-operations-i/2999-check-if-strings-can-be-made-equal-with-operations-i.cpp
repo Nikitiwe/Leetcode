989ba71e580fbc18b69e9c53261b31672afac152
Nikitiwe <rvnggm@gmail.com>
2025-04-29 17:10:54 +0700

    Time: 0 ms (100%), Space: 8.4 MB (38.75%) - LeetHub

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool canBeEqual(string s, string w) {
         if (s == w) return 1;
-        if (string(1, s[2]) + s[1] + s[0] + s[3] == w) return 1;
-        if (string(1, s[2]) + s[3] + s[0] + s[1] == w) return 1;
-        if (string(1, s[0]) + s[3] + s[2] + s[1] == w) return 1;
+        swap(s[0], s[2]);
+        if (s == w) return 1;
+        swap(s[1], s[3]);
+        if (s == w) return 1;
+        swap(s[0], s[2]);
+        if (s == w) return 1;
         return 0;
     }
 };
