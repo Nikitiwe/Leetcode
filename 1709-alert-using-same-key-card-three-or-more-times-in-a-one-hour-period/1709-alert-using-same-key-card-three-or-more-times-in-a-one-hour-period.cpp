24c3f4e0c9a75cf694740ad14cf59d0bae7fc7ce
Nikitiwe <rvnggm@gmail.com>
2025-04-30 12:56:40 +0700

    Time: 289 ms (7.14%), Space: 144.8 MB (10.39%) - LeetHub

--- 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
@@ -16,31 +16,42 @@ public:
             n[i] = v[i].second;
             t[i] = v[i].first;
         }
-        unordered_set<string> ans;
         unordered_map<string, int> id;
         int a = 0;
         vector<priority_queue<int>> arr;
+        vector<bool> isit;
         for (int i=0; i!=n.size(); i++)
         {
             if (id.count(n[i]) == 0)
             {
                 id[n[i]] = a;
                 arr.push_back(priority_queue<int>());
+                isit.push_back(0);
                 a++;
             }
-            if (arr[id[n[i]]].size() < 2) arr[id[n[i]]].push(f(t[i]));
-            else
+            if (isit[id[n[i]]] == 0)
             {
-                if (arr[id[n[i]]].size() == 2)
+                if (arr[id[n[i]]].size() < 2) arr[id[n[i]]].push(f(t[i]));
+                else
                 {
-                    if (arr[id[n[i]]].top() - 60 <= f(t[i]) ) ans.insert(n[i]);
-                    arr[id[n[i]]].pop();
-                    arr[id[n[i]]].push(f(t[i]));
+                    if (arr[id[n[i]]].size() == 2)
+                    {
+                        if (arr[id[n[i]]].top() - 60 <= f(t[i]) ) isit[id[n[i]]] = 1;
+                        arr[id[n[i]]].pop();
+                        arr[id[n[i]]].push(f(t[i]));
+                    }
                 }
             }
         }
         vector<string> res;
-        for (auto i = ans.begin(); i!= ans.end(); i++) res.push_back(*i);
+        for (int i=0; i!=n.size(); i++)
+        {
+            if (isit[id[n[i]]] == 1)
+            {
+                res.push_back(n[i]);
+                isit[id[n[i]]] = 0;
+            }
+        }
         sort(res.begin(), res.end());
         return res;
     }
