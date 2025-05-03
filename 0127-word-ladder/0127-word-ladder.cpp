17dc6fe5d188c6bad535b8cdfa552f733c7d0809
Nikitiwe <rvnggm@gmail.com>
2025-05-03 13:33:20 +0700

    Time: 588 ms (20.6%), Space: 17.9 MB (98.57%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& s) {
+        s.push_back(beginWord);
         vector<bool> isit(s.size(), 1);
-        queue<string> q;
+        queue<int> q;
         queue<int> l;
-        q.push(beginWord);
+        q.push(s.size()-1);
         l.push(1);
         while (q.size() > 0)
         {
-            string t = q.front();
+            int id = q.front();
             int ans = l.front();
             q.pop();
             l.pop();
@@ -17,13 +18,13 @@ public:
                 int c = 0;
                 for (int j=0; j!=s[i].size(); j++)
                 {
-                    if (t[j] != s[i][j]) c++;
+                    if (s[id][j] != s[i][j]) c++;
                 }
                 if (c == 1)
                 {
                     if (s[i] == endWord) return ans + 1;
                     isit[i] = 0;
-                    q.push(s[i]);
+                    q.push(i);
                     l.push(ans + 1);
                 }
             }
