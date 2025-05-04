e6754b20103218db1c8fd0172d813d582c01c9f5
Nikitiwe <rvnggm@gmail.com>
2025-05-04 13:00:12 +0700

    Time: 0 ms (100%), Space: 26 MB (92.25%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -5,8 +5,8 @@ public:
         unordered_map<int, int> m;
         for (int i=0; i!=d.size(); i++)
         {
-            int a=min(d[i][0], d[i][1]), b=max(d[i][0], d[i][1]);
-            ans+=m[10*b+a];
+            int a=min(d[i][0], d[i][1]), b = d[i][0] + d[i][1] - a;
+            ans += m[10*b+a];
             m[10*b+a]++;
         }
         return ans;
