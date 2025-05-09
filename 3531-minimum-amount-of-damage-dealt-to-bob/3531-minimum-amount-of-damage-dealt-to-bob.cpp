cdbd1266414a380abd51ecff9c0ce4004a011e9a
Nikitiwe <rvnggm@gmail.com>
2025-05-09 21:20:23 +0700

    Time: 63 ms (71.35%), Space: 142.3 MB (82.7%) - LeetHub

--- 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
+++ 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -3,22 +3,22 @@ public:
     long long minDamage(int p, vector<int>& d, vector<int>& h) {
         long long tt = 0;
         for (int i=0; i!=h.size(); i++) tt += (h[i] + p - 1)/p;
-        vector<vector<int>> arr(d.size(), vector<int>(2, 0));
+        vector<pair<int, int>> arr(d.size(), pair<int, int>());
         for (int i=0; i!=h.size(); i++)
         {
-            arr[i][0] = d[i];
-            arr[i][1] = h[i];
+            arr[i].first = d[i];
+            arr[i].second = h[i];
         }
         //sort(arr.begin(), arr.end(), ssort);
-        sort(arr.begin(), arr.end(), [p](const vector<int>& a, const vector<int>& b)
+        sort(arr.begin(), arr.end(), [p](const pair<int, int>& a, const pair<int, int>& b)
         {
-            return a[0] * ((b[1] + p - 1)/p) < b[0] * ((a[1] + p - 1)/p) ;
+            return a.first * ((b.second + p - 1)/p) < b.first * ((a.second + p - 1)/p) ;
         });
         long long ans = 0;
         for (int i=0; i!=arr.size(); i++)
         {
-            ans += tt * arr[i][0];
-            tt -= (arr[i][1] + p - 1)/p;
+            ans += tt * arr[i].first;
+            tt -= (arr[i].second + p - 1)/p;
         }
         return ans;
     }
