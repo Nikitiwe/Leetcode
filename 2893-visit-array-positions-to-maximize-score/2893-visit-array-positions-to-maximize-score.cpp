169343ae281a67ae48eb52de6e707340640c4fa1
Nikitiwe <rvnggm@gmail.com>
2025-05-10 13:49:43 +0700

    Time: 9 ms (61.29%), Space: 104.5 MB (62.21%) - LeetHub

--- 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.cpp
+++ 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums, int x) {
-        /*vector<long long> o(nums.size(), 0), e = o;
-        e[0] = nums[0];
-        o[0] = nums[0];*/
         long long mao = nums[0], mae = nums[0];
         if (nums[0] % 2 == 0) mao -= x;
         else mae -= x;
@@ -11,15 +8,11 @@ public:
         {
             if (nums[i] % 2 == 0)
             {
-                long long a = 0;
-                a = max(mae + nums[i], mao + nums[i] - x);
-                mae = max(mae, a);
+                mae = max(mae, max(mae + nums[i], mao + nums[i] - x));
             }
             else
             {
-                long long b = 0;
-                b = max(mao + nums[i], mae + nums[i] - x);
-                mao = max(mao, b);
+                mao = max(mao, max(mao + nums[i], mae + nums[i] - x));
             }
         }
         return max(mae, mao);
