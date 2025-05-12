530e29a6f6158d3f1ad5a9041aa41d9c4a37d834
Nikitiwe <rvnggm@gmail.com>
2025-05-12 14:07:26 +0700

    Time: 6 ms (62.12%), Space: 15.1 MB (34.56%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& d) {
-        unordered_set<int> m;
-        for (int i=0; i!=d.size(); i++)
-        {
-            for (int j=i+1; j<d.size(); j++)
-            {
-                for (int k=j+1; k<d.size(); k++)
-                {
-                    if (d[i]%2==0)
-                    {
-                        if (d[j]>0) m.insert(d[j]*100+d[k]*10+d[i]);
-                        if (d[k]>0) m.insert(d[j]*10+d[k]*100+d[i]);
-                    }
-                    if (d[j]%2==0)
-                    {
-                        if (d[i]>0) m.insert(d[i]*100+d[k]*10+d[j]);
-                        if (d[k]>0) m.insert(d[i]*10+d[k]*100+d[j]);
-                    }
-                    if (d[k]%2==0)
-                    {
-                        if (d[j]>0) m.insert(d[j]*100+d[i]*10+d[k]);
-                        if (d[i]>0) m.insert(d[j]*10+d[i]*100+d[k]);
-                    }
-                }
-            }
-        }
+        vector<int> arr(10, 0);
+        for (int i=0; i!=d.size(); i++) arr[d[i]]++;
         vector<int> ans;
-        for (auto i=m.begin(); i!=m.end(); i++) ans.push_back(*i);
-        sort(ans.begin(), ans.end());
+        for (int i=100; i<=999; i += 2)
+        {
+            bool isit = 1;
+            vector<int> t(10, 0);
+            t[i%10]++;
+            t[(i/10)%10]++;
+            t[(i/100)]++;
+            for (int j=0; j<10; j++) if (t[j] > arr[j])
+            {
+                isit = 0;
+                break;
+            }
+            if (isit == 1) ans.push_back(i);
+        }
         return ans;
     }
 };
