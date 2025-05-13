07291fa2d5afcf04dc274eb900ed061e47ab7fb0
Nikitiwe <rvnggm@gmail.com>
2025-05-13 13:18:31 +0700

    Time: 868 ms (23.65%), Space: 466.9 MB (40.39%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        vector<long> arr(26, 0), next(26, 0), zero(26, 0);
+        vector<long> arr(26, 0);
         long mod = 1e9 + 7;
         for (int i=0; i!=s.size(); i++) arr[s[i] - 'a']++;
         for (int k = 1; k<=t; k++)
         {
+            vector<long> next(26, 0);
             for (int i=0; i!=26; i++)
             {
                 next[(i+1)%26] += arr[i];
@@ -14,7 +15,6 @@ public:
             next[1] += arr[25];
             next[1] %= mod;
             arr = next;
-            next = zero;
         }
         long ans = 0;
         for (int i=0; i!=26; i++)
