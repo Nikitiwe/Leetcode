5fa243fabccdbf1f39dc6c1d7bfc3d28c6e54759
Nikitiwe <rvnggm@gmail.com>
2025-05-16 00:48:17 +0700

    Time: 120 ms (19.62%), Space: 90.5 MB (20.26%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        int id = 0;
+        map<int, int> s;
+        for (int t=0; t<=1e5; t++)
+        {
+            while (id < nums.size() && nums[id][0] <= t)
+            {
+                s[nums[id][1]]++;
+                id++;
+            }
+            if (s.size() > 0)
+            {
+                if (s.begin()->first < t) s.erase(s.begin()->first);
+                if (s.size() > 0)
+                {
+                    s[s.begin()->first]--;
+                    if (s[s.begin()->first] == 0) s.erase(s.begin()->first);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}; // [[1,5],[1,5],[1,5],[2,3],[2,3]]
