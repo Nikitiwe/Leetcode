e8fd50df29d337a88d97bdfafab45ae470b7d72f
Nikitiwe <rvnggm@gmail.com>
2025-05-16 01:10:48 +0700

    Time: 0 ms (100%), Space: 7.9 MB (37.29%) - LeetHub

--- 1936-maximize-number-of-nice-divisors/1936-maximize-number-of-nice-divisors.cpp
+++ 1936-maximize-number-of-nice-divisors/1936-maximize-number-of-nice-divisors.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int maxNiceDivisors(int k) {
-        long ans = 1, mod = 1e9 + 7;
-        while (k > 30000)
+        long long ans = 1, mod = 1e9 + 7;
+        while (k > 1000003)
         {
-            ans *= 895629451;
+            ans *= 154951791;
             ans %= mod;
-            k -= 30000;
+            k -= 999999;
+        }
+        while (k > 1002)
+        {
+            ans *= 965709895;
+            ans %= mod;
+            k -= 999;
         }
         while (k > 4)
         {
