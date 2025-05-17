d2ba3ff40a2bab1527319e091024b60a934df0f3
Nikitiwe <rvnggm@gmail.com>
2025-05-17 17:53:52 +0700

    Time: 8 ms (74.25%), Space: 44.2 MB (29.34%) - LeetHub

--- 3399-find-the-integer-added-to-array-ii/3399-find-the-integer-added-to-array-ii.cpp
+++ 3399-find-the-integer-added-to-array-ii/3399-find-the-integer-added-to-array-ii.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int minimumAddedInteger(vector<int>& nums, vector<int>& nums2) {
+        vector<int> s = nums;
+        sort(s.begin(), s.end());
+        int mi = 2000;
         vector<int> arr(1001, 0);
-        for (int i=0; i!=nums2.size(); i++) arr[nums2[i]]++;
-        for (int k = -1000; k<=1000; k++)
+        for (int i=0; i!=nums2.size(); i++)
         {
+            arr[nums2[i]]++;
+            mi = min(mi, nums2[i]);
+        }
+        for (int j = 2; j>=0; j--)
+        {
+            int k = mi - s[j];
             vector<int> t(1001, 0);
             for (int i=0; i!=nums.size(); i++) if (nums[i] + k >= 0 && nums[i] + k <= 1000) t[nums[i] + k]++;
             int c = 0;
