52cb616dbe59acb257959e7c177a3c8296fa8d21
Nikitiwe <rvnggm@gmail.com>
2025-05-18 02:51:19 +0700

    Time: 27 ms (97.12%), Space: 67.7 MB (94.6%) - LeetHub

---
+++ 1946-minimum-absolute-sum-difference/1946-minimum-absolute-sum-difference.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minAbsoluteSumDiff(vector<int>& nums, vector<int>& nums2) {
+        vector<int> arr = nums;
+        sort(arr.begin(), arr.end());
+        int add = 0;
+        for (int i=0; i!=nums.size(); i++)
+        {
+            int d = abs(nums[i] - nums2[i]);
+            if (add >= d) continue;
+            int l = 0, r = arr.size()-1, m, t = d;
+            while (l <= r)
+            {
+                m = (l+r)/2;
+                t = min(t, abs(arr[m] - nums2[i]));
+                if (arr[m] > nums2[i])
+                {
+                    r = m-1;
+                }
+                else l = m+1;
+            }
+            add = max(add, d - t);
+        }
+        long ans = -add;
+        for (int i=0; i!=nums.size(); i++) ans += abs(nums[i] - nums2[i]);
+        return ans % (1000000007);
+    }
+};
