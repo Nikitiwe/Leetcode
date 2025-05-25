5d985de2807c8d4f29edce22ab1b6ba76856e36a
Nikitiwe <rvnggm@gmail.com>
2025-05-26 01:30:42 +0700

    Time: 6 ms (97.74%), Space: 172.4 MB (12.55%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> arr;
+        vector<vector<int>> arr(26, vector<int>(26, 0));
         for (int i=0; i!=words.size(); i++)
         {
-            arr[words[i]]++;
+            arr[words[i][0] - 'a'][words[i][1] - 'a']++;
         }
-        char a = 'a', b;
         int ans = 0, ans1 = 0, ans2 = 0;
         for (int i=0; i!=26; i++)
         {
-            a = 'a' + i;
             for (int j=i+1; j<26; j++)
             {
-                
-                b = 'a' + j;
-                string temp{a, b}, temp1{b, a};
-                ans += min(arr[temp], arr[temp1]);
+                ans += min(arr[i][j], arr[j][i]);
             }
         }
         for (int i=0; i!=26; i++)
         {
-            a = 'a' + i;
-            string temp{a, a};
-            ans1 += arr[temp]/2;
-            if (ans2 == 0 && arr[temp]%2 == 1) {ans2 = 1;}
+            ans1 += arr[i][i]/2;
+            if (ans2 == 0 && arr[i][i]%2 == 1) {ans2 = 1;}
         }
         return 4*ans + 4*ans1 + 2*ans2;
     }
