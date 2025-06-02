ba99a0d67462583b1603e0a464a83071fd8ed8eb
Nikitiwe <rvnggm@gmail.com>
2025-06-02 23:32:22 +0700

    Time: 0 ms (100%), Space: 77.9 MB (72.68%) - LeetHub

--- 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     long long maximumTotalCost(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
-        //else if (nums.size() == 2) return max(nums[0]+nums[1], nums[0]-nums[1]);
-        vector<long long> p(nums.size(), 0), m = p;
-        p[0] = nums[0];
-        p[1] = nums[0]+nums[1];
-        m[1] = nums[0]-nums[1];
+        long long p = nums[0]+nums[1], m = nums[0]-nums[1];
         for (int i=2; i<nums.size(); i++)
         {
-            p[i] = max(p[i-1] + nums[i], m[i-1] + nums[i]);
-            m[i] = max(p[i-1] - nums[i], m[i-1] + nums[i]);
+            long long a = max(p + nums[i], m + nums[i]);
+            long long b = max(p - nums[i], m + nums[i]);
+            p = a;
+            m = b;
         }
-        return max(p.back(), m.back());
+        return max(p, m);
     }
 };
