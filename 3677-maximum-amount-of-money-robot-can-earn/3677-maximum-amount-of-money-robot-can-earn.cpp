7796afcafd581b89c1195f4f8679beb7d451ff7f
Nikitiwe <rvnggm@gmail.com>
2025-06-05 02:37:50 +0700

    Time: 545 ms (62.35%), Space: 275.8 MB (60.29%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -9,18 +9,31 @@ public:
         arr[0][0][2] = 0;
         for (int k=0; k<3; k++)
         {
-            for (int i=0; i!=c.size(); i++)
+            if (k == 0) for (int i=0; i!=c.size(); i++)
             {
                 for (int j=0; j!=c[0].size(); j++)
                 {
                     if (i > 0) arr[i][j][k] = arr[i-1][j][k] + c[i][j];
                     if (j > 0) arr[i][j][k] = max(arr[i][j][k], arr[i][j-1][k] + c[i][j]);
-                    if (k > 0 && i > 0) arr[i][j][k] = max(arr[i][j][k], arr[i-1][j][k-1]);
-                    if (k > 0 && j > 0) arr[i][j][k] = max(arr[i][j][k], arr[i][j-1][k-1]);
+                }
+            }
+            else for (int i=0; i!=c.size(); i++)
+            {
+                for (int j=0; j!=c[0].size(); j++)
+                {
+                    if (i > 0)
+                    {
+                        arr[i][j][k] = arr[i-1][j][k] + c[i][j];
+                        arr[i][j][k] = max(arr[i][j][k], arr[i-1][j][k-1]);
+                    }
+                    if (j > 0)
+                    {
+                        arr[i][j][k] = max(arr[i][j][k], arr[i][j-1][k] + c[i][j]);
+                        arr[i][j][k] = max(arr[i][j][k], arr[i][j-1][k-1]);
+                    }
                 }
             }
         }
-        //return arr[c.size()-1][c[0].size()-1][2];
         return max({arr[c.size()-1][c[0].size()-1][0], arr[c.size()-1][c[0].size()-1][1], arr[c.size()-1][c[0].size()-1][2]});
     }
 };
