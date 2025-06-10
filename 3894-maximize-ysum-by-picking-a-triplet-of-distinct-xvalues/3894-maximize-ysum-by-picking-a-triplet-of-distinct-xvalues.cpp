528462b4361c07a85174b56ea338c5006cfaf979
Nikitiwe <rvnggm@gmail.com>
2025-06-10 15:40:30 +0700

    Time: 102 ms (60.54%), Space: 194.7 MB (79.27%) - LeetHub

--- 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -7,9 +7,25 @@ public:
             m[x[i]] = max(m[x[i]], y[i]);
         }
         if (m.size() < 3) return -1;
-        vector<int> arr;
-        for (auto i = m.begin(); i!=m.end(); i++) arr.push_back(i->second);
-        sort(arr.rbegin(), arr.rend());
+        vector<int> arr(3, 0);
+        for (auto i = m.begin(); i!=m.end(); i++)
+        {
+            if (i->second >= arr[0])
+            {
+                arr[2] = arr[1];
+                arr[1] = arr[0];
+                arr[0] = i->second;
+            }
+            else if (i->second >= arr[1])
+            {
+                arr[2] = arr[1];
+                arr[1] = i->second;
+            }
+            else if (i->second >= arr[2])
+            {
+                arr[2] = i->second;
+            }
+        }
         return arr[0] + arr[1] + arr[2];
     }
 };
