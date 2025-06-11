8a35ff8c0a0e64e2baf0b0e093bab1531d1b863a
Nikitiwe <rvnggm@gmail.com>
2025-06-11 20:45:59 +0700

    Time: 422 ms (19.23%), Space: 320.9 MB (8.97%) - LeetHub

---
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int maxDifference(string s, int k) {
+        int ans = -100000;
+        for (int a = 0; a<=4; a++)
+        {
+            for (int b = 0; b<=4; b++)
+            {
+                if (a != b)
+                {
+                    priority_queue<tuple<int,int,int>> acbc, acbn, anbc, anbn;
+                    int ca = 0, cb = 0;
+                    vector<pair<int, int>> arr {{0,0}};
+                    for (int i=0; i!=s.size(); i++)
+                    {
+                        if (s[i] - '0' == a) ca++;
+                        else if (s[i] - '0' == b) cb++;
+
+                        arr.push_back({ca, cb});
+                        if (i >= k-1)
+                        {
+                            auto [x, y] = arr[i-k+1];
+                            if (x % 2 == 0 && y % 2 == 0) acbc.push({-x+y, -x, -y});
+                            else if (x % 2 == 0 && y % 2 == 1) acbn.push({-x+y, -x, -y});
+                            else if (x % 2 == 1 && y % 2 == 0) anbc.push({-x+y, -x, -y});
+                            else anbn.push({-x+y, -x, -y});
+                        }
+
+                        if (ca % 2 == 0 && cb % 2 == 0)
+                        {
+                            if (anbc.size() > 0)
+                            {
+                                auto [z, x, y] = anbc.top();
+                                x *=-1; y *= -1;
+                                if (ca - x > 0 && cb - y > 0) ans = max(ans, ca - x - cb + y);
+                            }
+                        }
+                        else if (ca % 2 == 0 && cb % 2 == 1)
+                        {
+                            if (anbn.size() > 0)
+                            {
+                                auto [z, x, y] = anbn.top();
+                                x *=-1; y *= -1;
+                                if (ca - x > 0 && cb - y > 0) ans = max(ans, ca - x - cb + y);
+                            }
+                        }
+                        else if (ca % 2 == 1 && cb % 2 == 0)
+                        {
+                            if (acbc.size() > 0)
+                            {
+                                auto [z, x, y] = acbc.top();
+                                x *=-1; y *= -1;
+                                if (ca - x > 0 && cb - y > 0) ans = max(ans, ca - x - cb + y);
+                            }
+                        }
+                        else
+                        {
+                            if (acbn.size() > 0)
+                            {
+                                auto [z, x, y] = acbn.top();
+                                x *=-1; y *= -1;
+                                if (ca - x > 0 && cb - y > 0) ans = max(ans, ca - x - cb + y);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
