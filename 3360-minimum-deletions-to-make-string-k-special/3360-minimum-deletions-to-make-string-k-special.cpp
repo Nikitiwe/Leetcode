6d41fe8b762d08d91dcd157632eea1d2129b3ba7
Nikitiwe <rvnggm@gmail.com>
2025-06-21 14:12:08 +0700

    Time: 4 ms (76.45%), Space: 19.3 MB (70.29%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> arr(26, 0);
         for (int i=0; i!=w.size(); i++) arr[w[i]-'a']++;
         sort(arr.begin(), arr.end());
-        int ans=1000000, s=0;
+        int ans = 1000000, s = 0;
         for (int i=0; i!=arr.size(); i++)
         {
             int sum=0;
             for (int j=i+1; j<arr.size(); j++)
             {
-                if (arr[j]>arr[i]+k) sum+=arr[j]-arr[i]-k;
+                if (arr[j]>arr[i]+k) sum += arr[j] - arr[i] - k;
             }
-            ans=min(ans, sum+s);
-            s+=arr[i];
+            ans = min(ans, sum+s);
+            s += arr[i];
         }
         return ans;
     }
