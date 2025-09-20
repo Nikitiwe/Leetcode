65a35a9e3a32f82caa9938deba3e780d60f8d6a0
Nikitiwe <rvnggm@gmail.com>
2025-09-21 03:13:59 +0700

    Time: 271 ms (91.61%), Space: 0B (100%) - LeetHub

---
+++ 4025-find-loyal-customers/4025-find-loyal-customers.sql
@@ -0,0 +1,18 @@
+/*WITH categorized_sales AS (
+    SELECT CASE WHEN MONTH(sale_date) IN (1, 2, 12) THEN 'Winter'
+                WHEN MONTH(sale_date) IN (3, 4, 5) THEN 'Spring'
+                WHEN MONTH(sale_date) IN (6, 7, 8) THEN 'Summer'
+                WHEN MONTH(sale_date) IN (9, 10, 11) THEN 'Fall' END AS season,
+        category,
+        SUM(quantity) AS total_quantity,
+        SUM(quantity * price) AS total_revenue
+    FROM sales AS t1 LEFT JOIN products AS t2 USING(product_id)
+    GROUP BY 1, 2
+)*/
+SELECT customer_id #, ROUND(1.0*SUM(IF(action = 'confirmed', 1, 0))/COUNT(*), 2) AS confirmation_rate
+FROM customer_transactions AS t1 #LEFT JOIN Confirmations AS t2 USING(user_id)
+GROUP BY 1
+HAVING SUM(IF(transaction_type = 'purchase', 1, 0)) >= 3 AND
+       DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30 AND
+       (1.0*SUM(IF(transaction_type = 'refund', 1, 0))/COUNT(*)) < 2.0/10
+ORDER BY customer_id ASC;
