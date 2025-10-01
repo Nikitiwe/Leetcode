4ff3bfafd19400050d7b6717e109c39a21fe3692
Nikitiwe <rvnggm@gmail.com>
2025-10-01 14:55:29 +0700

    Time: 0 ms (100%), Space: 7.7 MB (80.28%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int ans=0, t=0;
-        while (t+numBottles>=numExchange||numBottles>0)
+        while (t + numBottles >= numExchange || numBottles > 0)
         {
-            ans+=numBottles;
-            t+=numBottles;
-            numBottles=t/numExchange;
-            t=t%numExchange;
+            ans += numBottles;
+            t += numBottles;
+            numBottles = t / numExchange;
+            t %= numExchange;
         }
         return ans;
     }
